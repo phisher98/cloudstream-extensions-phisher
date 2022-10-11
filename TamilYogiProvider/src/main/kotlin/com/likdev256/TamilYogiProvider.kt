1825677456adaf47f26f5bd27d65625a57bfd939
LikDev-256 <lingamlikshan14@gmail.com>
2022-10-11 18:48:09 +0530

    A bundled fix, Homepage Done

--- TamilYogiProvider/src/main/kotlin/com/likdev256/TamilYogiProvider.kt
+++ TamilYogiProvider/src/main/kotlin/com/likdev256/TamilYogiProvider.kt
@@ -1,5 +1,6 @@
 package com.likdev256
 
+import android.util.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
@@ -19,6 +20,8 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
         TvType.TvSeries
     )
 
+    //private const val TAG = "TamilYogi"
+
     override val mainPage = mainPageOf(
         "$mainUrl/category/tamilyogi-full-movie-online/" to "New Movies",
         "$mainUrl/category/tamil-hd-movies/" to "HD Movies",
@@ -30,28 +33,31 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
         page: Int,
         request: MainPageRequest
     ): HomePageResponse {
-        print("request")
-        println(request)
-        print("Check")
-        println(request.data.removeSuffix("page/"))
-        val document = if (page == 1) {
-            app.get(request.data.removeSuffix("page/")).document
+        //Log.d("request", request.toString())
+        //Log.d("Check", request.data)
+        val document = app.get(request.data).document /* if (page == 1) {
+            app.get(request.data")).document
         } else {
             app.get(request.data + page).document
-        }
-        val home = document.select("div.cover").mapNotNull {
+        } */
+        //Log.d("CSS element", document.select("ul li").toString())
+        val home = document.select("ul li").mapNotNull {
             it.toSearchResult()
         }
+
+        return HomePageResponse(arrayListOf(HomePageList(request.name, home, isHorizontalImages = true)))
         return newHomePageResponse(request.name, home)
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("div.postcontent h2 a[title]")?.text()?.trim() ?: return null
-        println(title)
+        val titleS = this.selectFirst("div.cover a")?.attr("title")?.toString()?.trim() ?: return null
+        val titleRegex = Regex("(^.*\\)\\d*)")
+        val title = titleRegex.find(titleS)?.groups?.get(1)?.value.toString()
+        //Log.d("title", title)
         val href = fixUrl(this.selectFirst("a")?.attr("href").toString())
-        println(href)
+        //Log.d("href", href)
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
-        println(posterUrl)
+        //Log.d("posterUrl", posterUrl.toString())
 
         return newMovieSearchResponse(title, href, TvType.Movie) {
             this.posterUrl = posterUrl
@@ -60,50 +66,54 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
 
     override suspend fun search(query: String): List<SearchResponse> {
         val document = app.get("$mainUrl/?s=$query").document
+        Log.d("document", document.toString())
 
-        return document.select("div.cover").mapNotNull {
+        return document.select("ul li").mapNotNull {
             it.toSearchResult()
         }
     }
 
     override suspend fun load(url: String): LoadResponse? {
-        val document = app.get(url).document
-
-        val title = document.selectFirst("div.postcontent h2 a[title]")?.text()?.trim() ?: return null
-        println(title)
+        val doc = app.get(url).document
+        Log.d("Doc", doc.toString())
+        val titleL = doc.selectFirst("div.content h1 a")?.attr("title")?.toString()?.trim() ?: return null
+        val titleRegex = Regex("(^.*\\)\\d*)")
+        val title = titleRegex.find(titleL)?.groups?.get(1)?.value.toString()
+        Log.d("title", title)
         //val titleRegex = Regex()
         //val title =
-        val poster = fixUrlNull(document.selectFirst("img")?.attr("src"))
-        println(poster)
+        val poster = fixUrlNull(doc.selectFirst("img")?.attr("src"))
+        Log.d("poster", poster.toString())
         //val tags = document.select("div.mvici-left p:nth-child(1) a").map { it.text() }
         val yearRegex = Regex("\"(?<=\\()[\\d\\(\\]]+(?!=\\))\"")
         val year = yearRegex.find(title)?.groups?.get(1)?.value
             ?.toIntOrNull()
-        val tvType = if (document.selectFirst("div.les-content")
-                ?.select("a")?.size!! > 1 || document.selectFirst("ul.idTabs li strong")?.text()
+        Log.d("year", year.toString())
+        val tvType = if (doc.selectFirst("div.les-content")
+                ?.select("a")?.size!! > 1 || doc.selectFirst("ul.idTabs li strong")?.text()
                 ?.contains(Regex("(?i)(EP\\s?[0-9]+)|(episode\\s?[0-9]+)")) == true
         ) TvType.TvSeries else TvType.Movie
         //val description = document.selectFirst("p.f-desc")?.text()?.trim()
         //val trailer = fixUrlNull(document.select("iframe#iframe-trailer").attr("src"))
         //val rating = document.select("div.mvici-right > div.imdb_r span").text().toRatingInt()
         //val actors = document.select("div.mvici-left p:nth-child(3) a").map { it.text() }
-        val recommendations = document.select("div.ml-item").mapNotNull {
+        val recommendations = doc.select("ul li").mapNotNull {
             it.toSearchResult()
         }
 
         return if (tvType == TvType.TvSeries) {
-            val episodes = if (document.selectFirst("div.les-title strong")?.text().toString()
+            val episodes = if (doc.selectFirst("div.les-title strong")?.text().toString()
                     .contains(Regex("(?i)EP\\s?[0-9]+|Episode\\s?[0-9]+"))
             ) {
-                document.select("ul.idTabs li").map {
+                doc.select("ul.idTabs li").map {
                     val id = it.select("a").attr("href")
                     Episode(
-                        data = fixUrl(document.select("div$id iframe").attr("src")),
+                        data = fixUrl(doc.select("div$id iframe").attr("src")),
                         name = it.select("strong").text().replace("Server Ep", "Episode")
                     )
                 }
             } else {
-                document.select("div.les-content a").map {
+                doc.select("div.les-content a").map {
                     Episode(
                         data = it.attr("href"),
                         name = it.text().replace("Server Ep", "Episode").trim(),
@@ -143,11 +153,11 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
     ): Boolean {
 
         if (data.startsWith(mainUrl)) {
-            app.get(data).document.select("div.movieplay iframe").map { fixUrl(it.attr("src")) }
+            app.get(data).document.select(".entry > iframe:nth-child(3)").map { fixUrl(it.attr("src")) }
                 .apmap { source ->
                     safeApiCall {
                         when {
-                            source.startsWith("https://membed.net") -> app.get(
+                            source.startsWith("https://vembx.one") -> app.get(
                                 source,
                                 referer = "$mainUrl/"
                             ).document.select("ul.list-server-items li")
