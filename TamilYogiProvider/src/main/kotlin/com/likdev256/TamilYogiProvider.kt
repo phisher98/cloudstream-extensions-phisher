56d7cac3f24453382c3c7f40c54ec9dff9cb42b1
LikDev-256 <lingamlikshan14@gmail.com>
2022-10-18 22:19:41 +0530

    Fix no links found

--- TamilYogiProvider/src/main/kotlin/com/likdev256/TamilYogiProvider.kt
+++ TamilYogiProvider/src/main/kotlin/com/likdev256/TamilYogiProvider.kt
@@ -1,12 +1,12 @@
 package com.likdev256
 
-import android.util.Log
+//import android.util.Log
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
-import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
+//import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+//import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.mvvm.safeApiCall
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 
 class TamilYogiProvider : MainAPI() { // all providers must be an instance of MainAPI
@@ -59,7 +59,7 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
         //Log.d("href", href)
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
         //Log.d("posterUrl", posterUrl.toString())
-        val qualityRegex = Regex("(?i)((?:DVDRip)|(?:HD)|(?:HQ)|(?:HDRip))")
+        val qualityRegex = Regex("(?i)((DVDRip)|(HD)|(HQ)|(HDRip))")
         val qualityN = qualityRegex.find(titleS)?.value.toString()
         //Log.d("QualityN", qualityN)
         val quality = getQualityFromString(qualityN)
@@ -102,16 +102,16 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
         val titleRegex = Regex("(^.*\\)\\d*)")
         val rmGibRegex = Regex("(Permanent Link to )")
         val title = rmGibRegex.replace(titleRegex.find(titleL)?.groups?.get(1)?.value.toString(), "")
-        Log.d("title", title)
+        //Log.d("title", title)
         //val titleRegex = Regex()
         //val title =
         val poster = fixUrlNull(doc.selectFirst("meta[content\$=\".jpg\"]")?.attr("content"))
-        Log.d("poster", poster.toString())
+        //Log.d("poster", poster.toString())
         //val tags = document.select("div.mvici-left p:nth-child(1) a").map { it.text() }
         val yearRegex = Regex("(?<=\\()[\\d(\\]]+(?!=\\))")
         val year = yearRegex.find(title)?.value
             ?.toIntOrNull()
-        Log.d("year", year.toString())
+        //Log.d("year", year.toString())
         val checkTvSeriesRegex = Regex("(?i)(E\\s?[0-9]+)|([-]/?[0-9]+)")
         val tvType = if (title.contains(checkTvSeriesRegex))
             TvType.TvSeries else TvType.Movie
@@ -173,34 +173,44 @@ class TamilYogiProvider : MainAPI() { // all providers must be an instance of Ma
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-
-        if (data.startsWith(mainUrl)) {
-            app.get(data).document.select(".entry > iframe:nth-child(3)").map { fixUrl(it.attr("src")) }
-                .apmap { source ->
-                    safeApiCall {
-                        when {
-                            source.startsWith("https://vembx.one") -> app.get(
-                                source,
-                                referer = "$mainUrl/"
-                            ).document.select("ul.list-server-items li")
-                                .apmap {
-                                    loadExtractor(
-                                        it.attr("data-video").substringBefore("=https://msubload"),
-                                        "$mainUrl/",
-                                        subtitleCallback,
-                                        callback
-                                    )
-                                }
-                            else -> loadExtractor(source, "$mainUrl/", subtitleCallback, callback)
-                        }
-                    }
-                }
-        } else {
-            loadExtractor(data, "$mainUrl/", subtitleCallback, callback)
-        }
-
+        val linkRegex = Regex("(https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&\\/\\/=]*mp4))")
+        val source = app.get(data).document.select("div.entry iframe").attr("src")
+        val script = app.get(source, referer = "$mainUrl/").document.select("body > script").toString()
+        //val links = linkRegex.find(script)?.groups?.get(1)?.value.toString()
+        val links = linkRegex.findAll(script).map{it.value.trim()}.toList()
+        //Log.d("links", links.toString())
+            safeApiCall {
+                callback.invoke(
+                    ExtractorLink(
+                        "TamilYogi",
+                        "HD",
+                        links[0],
+                        "$mainUrl/",
+                        getQualityFromName("720p"),
+                        false
+                    )
+                )
+                callback.invoke(
+                    ExtractorLink(
+                        "TamilYogi",
+                        "SD",
+                        links[1],
+                        "$mainUrl/",
+                        getQualityFromName("480p"),
+                        false
+                    )
+                )
+                callback.invoke(
+                    ExtractorLink(
+                        "TamilYogi",
+                        "Low",
+                        links[2],
+                        "$mainUrl/",
+                        getQualityFromName("360p"),
+                        false
+                    )
+                )
+            }
         return true
     }
-
-
 }
