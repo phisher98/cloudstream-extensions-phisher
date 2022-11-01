f5f025d19174a3b44abb0c28fade37e292985ef6
LikDev-256 <lingamlikshan14@gmail.com>
2022-11-01 22:22:57 +0530

    Implemented movie loading

--- ShowFlixProvider/src/main/kotlin/com/likdev256/ShowFlixProvider.kt
+++ ShowFlixProvider/src/main/kotlin/com/likdev256/ShowFlixProvider.kt
@@ -1,6 +1,6 @@
 package com.likdev256
 
-//import android.util.Log
+import android.util.Log
 import com.lagradost.cloudstream3.*
 //import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.module.kotlin.*
@@ -9,12 +9,15 @@ import com.lagradost.nicehttp.RequestBodyTypes
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import com.fasterxml.jackson.annotation.*
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
+import kotlin.math.roundToInt
 
 class ShowFlixProvider : MainAPI() { // all providers must be an instance of MainAPI
     override var mainUrl = "https://showflix.in"
     override var name = "ShowFlix"
     override val hasMainPage = true
-    override var lang = "in"
+    override var lang = "ta"
     override val hasDownloadSupport = true
     override val supportedTypes = setOf(
         TvType.Movie,
@@ -24,7 +27,7 @@ class ShowFlixProvider : MainAPI() { // all providers must be an instance of Mai
     data class Results(
         @JsonProperty("objectId") var objectId: String,
         @JsonProperty("movieName") var movieName: String,
-        @JsonProperty("rating") var rating: String?,
+        @JsonProperty("rating") var rating: String,
         @JsonProperty("storyline") var storyline: String?,
         @JsonProperty("poster") var poster: String?,
         @JsonProperty("backdrop") var backdrop: String?,
@@ -58,8 +61,8 @@ class ShowFlixProvider : MainAPI() { // all providers must be an instance of Mai
             Pair("Tamil Dubbed TV Series", "$mainUrl/lan/series/Tamil Dubbed")
         )*/
 
-    val MovieapiUrl = "https://parse.showflix.tk/parse/classes/movies"
-    val TVapiUrl = "https://parse.showflix.tk/parse/classes/series"
+    private val MovieapiUrl = "https://parse.showflix.tk/parse/classes/movies"
+    private val TVapiUrl = "https://parse.showflix.tk/parse/classes/series"
 
     private suspend fun queryMovieApi(skip: Int, query: String): NiceResponse {
         val req =
@@ -122,15 +125,72 @@ class ShowFlixProvider : MainAPI() { // all providers must be an instance of Mai
                 home.add(
                     newMovieSearchResponse(
                         it.movieName,
-                        url = "$mainUrl/movie/${it.objectId}",
+                        it.objectId,
                         TvType.Movie
                     ) {
                         this.posterUrl = it.poster
-                        //this.quality = quality
+                        this.quality = getQualityFromString("hd")
                     })
             }
             elements.add(HomePageList(Name, home))
         }
         return HomePageResponse(elements, hasNext = true)
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val MovieSearchreq =
+            """{"where":{"movieName":{"${"$"}regex":"$query","${"$"}options":"i"}},"order":"-updatedAt","_method":"GET","_ApplicationId":"SHOWFLIXAPPID","_ClientVersion":"js3.4.1","_InstallationId":"1c380d0e-7415-433c-b0ab-675872a0e782"}""".toRequestBody(
+                RequestBodyTypes.JSON.toMediaTypeOrNull()
+            )
+        val TVSearchreq =
+            """{"where":{"seriesName":{"${"$"}regex":"$query","${"$"}options":"i"}},"order":"-updatedAt","_method":"GET","_ApplicationId":"SHOWFLIXAPPID","_ClientVersion":"js3.4.1","_InstallationId":"1c380d0e-7415-433c-b0ab-675872a0e782"}""".toRequestBody(
+                RequestBodyTypes.JSON.toMediaTypeOrNull()
+            )
+        //Log.d("JSON", res.toString())
+        return app.post(MovieapiUrl, requestBody = MovieSearchreq, referer = "https://showflix.in/").parsed<All>().results.map {
+            newMovieSearchResponse(it.movieName, it.objectId, TvType.Movie) {
+                this.posterUrl = it.poster
+                this.quality = getQualityFromString("hd")
+            }
+        }
+    }
+
+    override suspend fun load(url: String): LoadResponse? {
+        val Regexurl = Regex("(https:\\/\\/showflix\\.in/)")
+        val objID = Regexurl.replace(url, "")
+        val MovieLoadreq =
+            """{"where":{"objectId":"$objID"},"limit":1,"_method":"GET","_ApplicationId":"SHOWFLIXAPPID","_ClientVersion":"js3.4.1","_InstallationId":"1c380d0e-7415-433c-b0ab-675872a0e782"}""".toRequestBody(
+                RequestBodyTypes.JSON.toMediaTypeOrNull()
+            )
+        Log.d("rq", objID.toString())
+        val resp = app.post(MovieapiUrl, requestBody = MovieLoadreq, referer = "https://showflix.in/").toString()
+        val Regexlist1 = Regex("(\\{\"results\":\\[)")
+        val Regexlist2 = Regex("(?<=\\})(?:(.)*)*")
+        val clean1 = Regexlist1.replace(resp, "")
+        Log.d("res", resp)
+        Log.d("test1", clean1)
+        val clean2 = Regexlist2.replace(clean1, "")
+        Log.d("test", clean2)
+        val it = parseJson<Results>(clean2)
+        val title = it.movieName
+        val yearRegex = Regex("(?<=\\()[\\d(\\]]+(?!=\\))")
+        val year = yearRegex.find(title)?.value
+            ?.toIntOrNull()
+        val poster = it.poster
+        val backdrop = it.backdrop
+        val plot = it.storyline
+        val rating = it.rating.toDouble().roundToInt()
+        //val recommendations = app.post(MovieapiUrl, requestBody = MovieLoadreq, referer = "https://showflix.in/").parsed<All>().results
+        return newMovieLoadResponse(title, "$mainUrl${"/"}movie${"/"}${it.objectId}", TvType.Movie, "$mainUrl${"/"}movie${"/"}${it.objectId}") {
+                this.posterUrl = poster
+                this.year = year
+                this.plot = plot
+                //this.tags = tags
+                this.rating = rating
+                this.backgroundPosterUrl = backdrop
+                //addActors(actors)
+                //this.recommendations = recommendations
+                //addTrailer(trailer)
+            }
     }
 }
