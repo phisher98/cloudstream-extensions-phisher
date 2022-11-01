1b8970a5103e33502f2559879ffb18b21cb8b811
LikDev-256 <lingamlikshan14@gmail.com>
2022-11-01 11:17:10 +0530

    Improve Showflix Homepage

--- ShowFlixProvider/src/main/kotlin/com/likdev256/ShowFlixProvider.kt
+++ ShowFlixProvider/src/main/kotlin/com/likdev256/ShowFlixProvider.kt
@@ -1,8 +1,8 @@
 package com.likdev256
 
-import android.util.Log
+//import android.util.Log
 import com.lagradost.cloudstream3.*
-import com.fasterxml.jackson.annotation.JsonProperty
+//import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.module.kotlin.*
 import com.lagradost.nicehttp.NiceResponse
 import com.lagradost.nicehttp.RequestBodyTypes
@@ -14,29 +14,12 @@ class ShowFlixProvider : MainAPI() { // all providers must be an instance of Mai
     override var mainUrl = "https://showflix.in"
     override var name = "ShowFlix"
     override val hasMainPage = true
-    override var lang = "ta"
+    override var lang = "in"
     override val hasDownloadSupport = true
     override val supportedTypes = setOf(
         TvType.Movie,
         TvType.TvSeries
     )
-
-    //private const val TAG = "ShowFlix"
-
-    override val mainPage = mainPageOf(
-        "" to "Trending Movies",
-        """\\QTamil\\E""" to "Tamil Movies",
-        """\\QTamil Dubbed\\E""" to "Dubbed Movies",
-        """\\QEnglish\\E""" to "English Movies",
-        """\\QTelugu\\E""" to "Telugu Movies",
-        """\\QHindi\\E""" to "Hindi Movies",
-        """\\QMalayalam\\E""" to "Malayalam Movies",
-        "$mainUrl/lan/series/Tamil Dubbed" to "Dubbed TV Series"
-    )
-
-    //Log.d("request", request.toString())
-    //Log.d("Check", request.data)
-    //Log.d("Page", page.toString())
 
     data class Results(
         @JsonProperty("objectId") var objectId: String,
@@ -58,7 +41,12 @@ class ShowFlixProvider : MainAPI() { // all providers must be an instance of Mai
         @JsonProperty("results") var results: ArrayList<Results> = arrayListOf()
     )
 
-      /* val elements = listOf(
+    data class fullCount(
+        @JsonProperty("results") var results : ArrayList<String> = arrayListOf(),
+        @JsonProperty("count") var count : Int
+    )
+
+    /* val elements = listOf(
             Pair("Trending Now", "$mainUrl/movie"),
             Pair("Tamil Movies", "$mainUrl/lan/movie/Tamil"),
             Pair("Tamil Dubbed Movies", "$mainUrl/lan/series/Tamil Dubbed"),
@@ -70,51 +58,79 @@ class ShowFlixProvider : MainAPI() { // all providers must be an instance of Mai
             Pair("Tamil Dubbed TV Series", "$mainUrl/lan/series/Tamil Dubbed")
         )*/
 
-    private suspend fun queryApi(apiUrl: String, query: String): NiceResponse {
-        //val res = app.post(
-            //"https://parse.showflix.tk/parse/classes/movies",
-            //requestBody = """{"where":{"category":{"${"$"}regex":"\\QTamil Dubbed\\E"}},"limit":50,"order":"-updatedAt","_method":"GET","_ApplicationId":"SHOWFLIXAPPID","_ClientVersion":"js3.4.1","_InstallationId":"1c380d0e-7415-433c-b0ab-675872a0e782"}""".toRequestBody(RequestBodyTypes.JSON.toMediaTypeOrNull()),
-            //referer = "https://showflix.in/"
-        //).parsed<All>()
-        val req = """{"where":{"category":{"${"$"}regex":"$query"}},"limit":50,"order":"-updatedAt","_method":"GET","_ApplicationId":"SHOWFLIXAPPID","_ClientVersion":"js3.4.1","_InstallationId":"1c380d0e-7415-433c-b0ab-675872a0e782"}""".toRequestBody(RequestBodyTypes.JSON.toMediaTypeOrNull())
+    val MovieapiUrl = "https://parse.showflix.tk/parse/classes/movies"
+    val TVapiUrl = "https://parse.showflix.tk/parse/classes/series"
 
+    private suspend fun queryMovieApi(skip: Int, query: String): NiceResponse {
+        val req =
+            """{"where":{"category":{"${"$"}regex":"$query"}},"limit":20,"skip":$skip,"_method":"GET","_ApplicationId":"SHOWFLIXAPPID","_ClientVersion":"js3.4.1","_InstallationId":"1c380d0e-7415-433c-b0ab-675872a0e782"}""".toRequestBody(
+            RequestBodyTypes.JSON.toMediaTypeOrNull()
+            )
+            //Log.d("JSON", res.toString())
+        return app.post(
+            MovieapiUrl,
+            requestBody = req,
+            referer = "https://showflix.in/"
+        )
+    }
+
+    private suspend fun queryTVApi(skip: Int, query: String): NiceResponse {
+        val req =
+            """{"where":{"category":{"${"$"}regex":"$query"}},"limit":20,"skip":$skip,"_method":"GET","_ApplicationId":"SHOWFLIXAPPID","_ClientVersion":"js3.4.1","_InstallationId":"1c380d0e-7415-433c-b0ab-675872a0e782"}""".toRequestBody(
+                RequestBodyTypes.JSON.toMediaTypeOrNull()
+            )
         //Log.d("JSON", res.toString())
-        return app.post(apiUrl, requestBody = req, referer = "https://showflix.in/")
+        return app.post(
+            TVapiUrl,
+            requestBody = req,
+            referer = "https://showflix.in/"
+        )
     }
 
     override suspend fun getMainPage(
         page: Int,
         request: MainPageRequest
     ): HomePageResponse {
+        val Moviereq =
+            """{"where":{},"limit":0,"count":1,"_method":"GET","_ApplicationId":"SHOWFLIXAPPID","_ClientVersion":"js3.4.1","_InstallationId":"1c380d0e-7415-433c-b0ab-675872a0e782"}""".toRequestBody(
+                RequestBodyTypes.JSON.toMediaTypeOrNull()
+            )
+        val TVreq =
+            """{"where":{},"limit":0,"count":1,"_method":"GET","_ApplicationId":"SHOWFLIXAPPID","_ClientVersion":"js3.4.1","_InstallationId":"1c380d0e-7415-433c-b0ab-675872a0e782"}""".toRequestBody(
+                RequestBodyTypes.JSON.toMediaTypeOrNull()
+            )
+        val fullMovies = app.post(MovieapiUrl, requestBody = Moviereq, referer = "https://showflix.in/").parsed<fullCount>().count
+        val fullTV = app.post(TVapiUrl, requestBody = TVreq, referer = "https://showflix.in/").parsed<fullCount>().count
+        val mainpage = listOf(
+            Pair("", "Trending Movies"),
+            Pair("""\\QTamil\\E""", "Tamil Movies"),
+            Pair("""\\QTamil Dubbed\\E""", "Dubbed Movies"),
+            Pair("""\\QEnglish\\E""", "English Movies"),
+            Pair("""\\QTelugu\\E""", "Telugu Movies"),
+            Pair("""\\QHindi\\E""", "Hindi Movies"),
+            Pair("""\\QMalayalam\\E""", "Malayalam Movies")
+        )
+        val elements = ArrayList<HomePageList>()
 
-        /*val req = mapOf(
-            "_ApplicationId" to "SHOWFLIXAPPID",
-            "_ClientVersion" to "js3.4.1",
-            "_InstallationId" to "2573d2b7-ef8f-48f0-9e91-8e5c2beb220d",
-            "_method" to "GET",
-            "limit" to "50",
-            "order" to "-updatedAt",
-            "where" to mapOf("category" to mapOf("$/regex" to "\\QTamil Dubbed\\E"))
-        )*/
-
-        val list = queryApi("https://parse.showflix.tk/parse/classes/movies", "").parsed<All>().results.map {
-            newMovieSearchResponse(
-                it.movieName,
-                url = "$mainUrl/movie/${it.objectId}",
-                TvType.Movie
-            ) {
-                this.posterUrl = it.poster
-                //this.quality = quality
+        mainpage.apmap { (query, Name) ->
+            val home = ArrayList<SearchResponse>()
+            val list = queryMovieApi(
+                page*10,
+                query
+            ).parsed<All>().results
+            list.map {
+                home.add(
+                    newMovieSearchResponse(
+                        it.movieName,
+                        url = "$mainUrl/movie/${it.objectId}",
+                        TvType.Movie
+                    ) {
+                        this.posterUrl = it.poster
+                        //this.quality = quality
+                    })
             }
+            elements.add(HomePageList(Name, home))
         }
-        return if (request.name.contains("(H)")) HomePageResponse(
-            arrayListOf(
-                HomePageList(
-                    request.name.replace(" (H)", ""),
-                    list,
-                    request.name.contains("(H)")
-                )
-            ), hasNext = true)
-        else newHomePageResponse(request.name, list)
+        return HomePageResponse(elements, hasNext = true)
     }
 }
