2f702eac9e7ce421ef02ead5cfda22a823054b7c
LikDev-256 <81100289+LikDev-256@users.noreply.github.com>
2022-11-04 14:59:40 +0530

    Test fix

--- ShowFlixProvider/src/main/kotlin/com/likdev256/ShowFlixProvider.kt
+++ ShowFlixProvider/src/main/kotlin/com/likdev256/ShowFlixProvider.kt
@@ -240,7 +240,7 @@ class ShowFlixProvider : MainAPI() { // all providers must be an instance of Mai
                     this.quality = SearchQuality.HD
                 }
             }
-        val both = TVSeries //Movies + TVSeries
+        val both = Movies + TVSeries
         return both //.sortedBy { -FuzzySearch.partialRatio(it.name.lowercase(), query.lowercase()) }
 }
 
