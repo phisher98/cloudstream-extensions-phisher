fcc17b60c460246810495dca2a787fc743dc2be6
LikDev-256 <81100289+LikDev-256@users.noreply.github.com>
2022-11-10 21:53:09 +0530

    Version Bump general fixes

--- NOXXProvider/build.gradle.kts
+++ NOXXProvider/build.gradle.kts
@@ -1,11 +1,11 @@
 // use an integer for version numbers
-version = 1
+version = 3
 
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "HD Tv Shows"
+    description = "HD TV Shows"
     language = "en"
     authors = listOf("LikDev-256")
 
