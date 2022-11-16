d99748e924107b01c58b1df1cc5102633ef32ca9
LikDev-256 <lingamlikshan14@gmail.com>
2022-11-16 23:05:23 +0530

    Add new provider MovieHUB

--- MovieHUBProvider/src/main/kotlin/com/likdev256/MovieHUBProvider.kt
+++ MovieHUBProvider/src/main/kotlin/com/likdev256/MovieHUBProvider.kt
@@ -2,7 +2,7 @@ package com.likdev256
 
 import android.util.Log
 import com.lagradost.cloudstream3.*
-//import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 //import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.mvvm.safeApiCall
 import com.lagradost.cloudstream3.utils.ExtractorLink
@@ -11,21 +11,29 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import org.jsoup.nodes.Element
 import com.lagradost.nicehttp.NiceResponse
+import me.xdrop.fuzzywuzzy.FuzzySearch
 
-class Av1EncodedProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://www.av1encoded.in"
-    override var name = "Av1Encoded"
+class MovieHUBProvider : MainAPI() { // all providers must be an instance of MainAPI
+    override var mainUrl = "https://x265rips.co"
+    override var name = "MovieHUB"
     override val hasMainPage = true
     override var lang = "ta"
     override val hasDownloadSupport = true
     override val supportedTypes = setOf(
-        TvType.Movie
+        TvType.Movie,
+        TvType.TvSeries
     )
 
     override val mainPage = mainPageOf(
-        "$mainUrl/?order=years-desc" to "Recent Movies",
-        "$mainUrl/?order=rating" to "TopRated Movies",
-        "$mainUrl/?order=title-asc" to "Movies"
+        "$mainUrl/movies/" to "Movies",
+        "$mainUrl/series/" to "TV Shows",
+        "$mainUrl/genre/action/" to "Action",
+        "$mainUrl/genre/adventure/" to "Adventure",
+        "$mainUrl/genre/animation/" to "Animation",
+        "$mainUrl/genre/fantasy/" to "Fantasy",
+        "$mainUrl/genre/horror/" to "Horror",
+        "$mainUrl/genre/romance/" to "Romance",
+        "$mainUrl/genre/science-fiction/" to "Sci-Fi"
     )
 
     override suspend fun getMainPage(
@@ -35,27 +43,34 @@ class Av1EncodedProvider : MainAPI() { // all providers must be an instance of M
         val document = if (page == 1) {
             app.get(request.data).document
         } else {
-            app.get(request.data.removeSuffix(request.data.format(page).removePrefix(mainUrl))
-                    + "/page/" + page + request.data.format(page).removePrefix(mainUrl) ).document
+            app.get(request.data + "/page/$page/").document
         }
-        //Log.d("request", request.data.removeSuffix(request.data.format(page).removePrefix(mainUrl))
-        //        + "/page/" + page + request.data.format(page).removePrefix(mainUrl))
-        //Log.d("CSS element", document.select("div.item-container > div").toString())
-        val home = document.select("div.item-container > div").mapNotNull {
-            it.toSearchResult()
+
+        Log.d("Document", request.data)
+
+        val home = if (request.data.contains("genre")) {
+            document.select("div.items > article").mapNotNull {
+                it.toSearchResult()
+            }
+        } else {
+            document.select("#archive-content > article").mapNotNull {
+                it.toSearchResult()
+            }
         }
+
         return HomePageResponse(arrayListOf(HomePageList(request.name, home)), hasNext = true)
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
-        val title = this.selectFirst("h2.movie-title")?.toString()?.removePrefix("<h2 class=\"movie-title\">")?.removeSuffix("</h2>")?.trim() ?: return null
-        //Log.d("title", titleS)
-        val href = fixUrl(this.selectFirst("a")?.attr("href").toString())
+        //Log.d("Got","got here")
+        val title = this.selectFirst("div.animation-1 > div.title > h4")?.text()?.toString()?.trim() ?: return null
+        //Log.d("title", title)
+        val href = fixUrl(this.selectFirst("div.poster > a")?.attr("href").toString())
         //Log.d("href", href)
-        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-src"))
+        val posterUrl = fixUrlNull(this.selectFirst("div.poster > img")?.attr("data-src"))
         //Log.d("posterUrl", posterUrl.toString())
         //Log.d("QualityN", qualityN)
-        val quality = SearchQuality.HD
+        val quality = getQualityFromString(this.select("div.poster > div.mepo > span").text().toString())
         //Log.d("Quality", quality.toString())
         return newMovieSearchResponse(title, href, TvType.Movie) {
             this.posterUrl = posterUrl
@@ -67,44 +82,76 @@ class Av1EncodedProvider : MainAPI() { // all providers must be an instance of M
         val document = app.get("$mainUrl/?s=$query").document
         //Log.d("document", document.toString())
 
-        return document.select("div.item-container > div").mapNotNull {
-            it.toSearchResult()
+        val searchResponse = document.select("div.result-item").mapNotNull {
+            val title = it.selectFirst("article > div.details > div.title > a")?.text().toString().trim()
+            //Log.d("title", titleS)
+            val href = fixUrl(it.selectFirst("article > div.details > div.title > a")?.attr("href").toString())
+            //Log.d("href", href)
+            val posterUrl = fixUrlNull(it.selectFirst("article > div.image > div.thumbnail > a > img")?.attr("src"))
+            //Log.d("posterUrl", posterUrl.toString())
+            //Log.d("QualityN", qualityN)
+            val quality = getQualityFromString(it.select("div.poster > div.mepo > span").text().toString())
+            //Log.d("Quality", quality.toString())
+            val type = it.select("article > div.image > div.thumbnail > a > span").text().toString()
+            if (type.contains("Movie")) {
+                newMovieSearchResponse(title, href, TvType.Movie) {
+                    this.posterUrl = posterUrl
+                    this.quality = quality
+                }
+            } else {
+                newTvSeriesSearchResponse(title, href, TvType.TvSeries) {
+                    this.posterUrl = posterUrl
+                    this.quality = quality
+                }
+            }
         }
+        return searchResponse
     }
 
     override suspend fun load(url: String): LoadResponse? {
         val doc = app.get(url).document
         //Log.d("Doc", doc.toString())
-        val title = doc.selectFirst("h1.entry-title")?.text()?.toString()?.trim() ?: return null
-        //val titleRegex = Regex("(^.*\\)\\d*)")
-        //val title = titleRegex.find(titleL)?.groups?.get(1)?.value.toString()
+        val titleL = doc.selectFirst("div.sheader > div.data > h1")?.text()?.toString()?.trim() ?: return null
+        val titleRegex = Regex("(^.*\\)\\d*)")
+        val title = titleRegex.find(titleL)?.groups?.get(1)?.value.toString()
         //Log.d("titleL", titleL)
         //Log.d("title", title)
-        val poster = fixUrlNull(doc.selectFirst("img")?.attr("data-lazy-src"))
+        val poster = fixUrlNull(doc.selectFirst("div.poster > img")?.attr("src"))
+        val bgposter = fixUrlNull(doc.selectFirst("#dt_galery > div.owl-wrapper-outer > div.owl-wrapper > div.owl-item > div.g-item > a")?.attr("href"))
         //Log.d("poster", poster.toString())
-        val tags = doc.select("div.details > span[itemprop*=\"genre\"] > a").map { it.text() }
-        val year = doc.selectFirst("div.details > span > a")?.text()?.toString()?.toInt()
+        val tags = doc.select("div.sgeneros > a").map { it.text() }
+        //val year = doc.selectFirst("span.date")?.text()?.toString()?.split(",")?.toString()?.toInt()
         //Log.d("year", year.toString())
-        val description = doc.selectFirst("p.movie-description span.trama")?.text()?.trim()
+        val description = doc.selectFirst("div.wp-content > p > span")?.text()?.trim()
         //Log.d("desc", description.toString())
         val trailer = fixUrlNull(doc.select("#trailer > a").attr("href"))
         //Log.d("trailer", trailer.toString())
-        val rating = doc.select("span.progress-value").text().toRatingInt()
+        val rating = doc.select("span.dt_rating_vgs").text().toRatingInt()
         //Log.d("rating", rating.toString())
-        val duration = doc.selectFirst("div.details > span[itemprop*=\"duration\"]")?.text()?.toString()?.removeSuffix("min")?.trim()?.toInt()
+        val duration = doc.selectFirst("span.runtime")?.text()?.toString()?.removeSuffix(" Min.")?.trim()?.toInt()
         //Log.d("dur", duration.toString())
-        val actors = doc.select("div.person > div.data > div.name > a").map { it.text() }
+        val actors =
+            doc.select("div.person").map{
+                ActorData(
+                    Actor(
+                        it.select("div.data > div.name > a").text().toString(),
+                        it.select("div.img > a > img").attr("src").toString()
+                    ),
+                    roleString = it.select("div.data > div.caracter").text().toString(),
+                )
+            }
         val recommendations = doc.select("div.item-container > div").mapNotNull {
             it.toSearchResult()
         }
         return newMovieLoadResponse(title, url, TvType.Movie, url) {
                 this.posterUrl = poster
+                this.backgroundPosterUrl = bgposter
                 this.year = year
                 this.plot = description
                 this.tags = tags
                 this.rating = rating
                 this.duration = duration
-                addActors(actors)
+                this.actors = actors
                 this.recommendations = recommendations
                 addTrailer(trailer)
             }
--- MovieHUBProvider/src/main/kotlin/com/likdev256/MovieHUBProviderPlugin.kt
+++ MovieHUBProvider/src/main/kotlin/com/likdev256/MovieHUBProviderPlugin.kt
@@ -5,9 +5,9 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class Av1EncodedProviderPlugin: Plugin() {
+class MovieHUBProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(Av1EncodedProvider())
+        registerMainAPI(MovieHUBProvider())
     }
 }
