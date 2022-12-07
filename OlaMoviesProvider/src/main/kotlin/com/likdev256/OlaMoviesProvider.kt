fa2b43956ccbdb51e93f1d0044249886e2a8e093
LikDev-256 <lingamlikshan14@gmail.com>
2022-12-08 00:49:49 +0530

    code cursed af

--- OlaMoviesProvider/src/main/kotlin/com/likdev256/OlaMoviesProvider.kt
+++ OlaMoviesProvider/src/main/kotlin/com/likdev256/OlaMoviesProvider.kt
@@ -6,17 +6,19 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.mvvm.safeApiCall
-import com.lagradost.cloudstream3.utils.AppUtils.toJson
+import com.lagradost.cloudstream3.utils.AppUtils
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.getQualityFromName
-import com.lagradost.nicehttp.JsonAsString
 import com.lagradost.nicehttp.RequestBodyTypes
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import okhttp3.FormBody
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.jsoup.nodes.Element
+import java.net.URI
 
 class OlaMoviesProvider : MainAPI() { // all providers must be an instance of MainAPI
     override var mainUrl = "https://olamovies.cyou"
@@ -27,6 +29,16 @@ class OlaMoviesProvider : MainAPI() { // all providers must be an instance of Ma
     override val supportedTypes = setOf(
         TvType.Movie,
         TvType.TvSeries
+    )
+
+    data class Links(
+        @JsonProperty("link") val link: String,
+        @JsonProperty("quality") val quality: String
+    )
+
+    data class MovieLinks(
+        @JsonProperty("url") val url: String,
+        @JsonProperty("list") val list: List<Links> = arrayListOf()
     )
 
     override val mainPage = mainPageOf(
@@ -122,6 +134,13 @@ class OlaMoviesProvider : MainAPI() { // all providers must be an instance of Ma
         }
     }
 
+    private fun String.containsAnyOfIgnoreCase(keywords: List<String>): Boolean {
+        for (keyword in keywords) {
+            if (this.contains(keyword, true)) return true
+        }
+        return false
+    }
+
     override suspend fun load(url: String): LoadResponse {
         val doc = app.get(url).document
         //Log.d("Doc", doc.toString())
@@ -130,17 +149,14 @@ class OlaMoviesProvider : MainAPI() { // all providers must be an instance of Ma
         val title = titleRegex.find(titleS)?.groups?.get(1)?.value.toString()
         //Log.d("title", title)
         val href = doc.select("div.is-layout-flex > div.wp-block-button").map {
-            mainUrl + it.select("a[target=_blank]").attr("href") + "<?>" + it.select("a[target=_blank]").text().toString()
+            Links(
+                link = "$mainUrl${it.select("a[target=_blank]").attr("href")}",
+                quality = it.select("a[target=_blank]").text()
+            )
         }
         //Log.d("href", href)
         val poster = fixUrlNull(doc.select("span.gridlove-cover > a").attr("href"))
         //Log.d("poster", poster.toString())
-        fun String.containsAnyOfIgnoreCase(keywords: List<String>): Boolean {
-            for (keyword in keywords) {
-                if (this.contains(keyword, true)) return true
-            }
-            return false
-        }
         val bloat = listOf("720p","1080p","2160p","Bluray","x264","x265","60FPS","120fps","144FPS","WEB-DL")
         val tags = doc.select("div.entry-tags > a").map { if (it.text().containsAnyOfIgnoreCase(bloat)) "" else it.text() }.filter { !it.isNullOrBlank() }
         val yearRegex = Regex("(?<=\\()[\\d(\\]]+(?!=\\))")
@@ -197,7 +213,7 @@ class OlaMoviesProvider : MainAPI() { // all providers must be an instance of Ma
         }
 
         return if (type.contains("Movies")) {
-            newMovieLoadResponse(title, url, TvType.Movie, href) {
+            newMovieLoadResponse(title, url, TvType.Movie, MovieLinks(url, href)) {
                 this.posterUrl = poster
                 this.year = year
                 this.tags = tags
@@ -227,38 +243,146 @@ class OlaMoviesProvider : MainAPI() { // all providers must be an instance of Ma
 
     private suspend fun bypassAdLinks(link: String): String? {
         val apiUrl = "https://api.emilyx.in/api/bypass"
+        val apiSupportedLinks = listOf("dulink", "ez4short")
+
         val type = if (link.contains("rocklinks")) "rocklinks"
-            else if (link.contains("dulink")) "dulink"
-            else if (link.contains("ez4short")) "ez4short"
-            else ""
+        else if (link.contains("dulink")) "dulink"
+        else if (link.contains("ez4short")) "ez4short"
+        else ""
         //Log.d("mytype", type)
         val values = mapOf("type" to type, "url" to link)
         //Log.d("mytype", values.toString())
-        val json = mapper.writeValueAsString(values).toRequestBody(RequestBodyTypes.JSON.toMediaTypeOrNull())
-       // Log.d("mytype", json.toString())
+        val json = mapper.writeValueAsString(values)
+            .toRequestBody(RequestBodyTypes.JSON.toMediaTypeOrNull())
+        // Log.d("mytype", json.toString())
         //Log.d("mytype", JsonAsString(json).toString())
-        return coroutineScope {
-                    val asyncTask = async {
-                        app.post(
-                        url = apiUrl,
-                        requestBody = json
-                        ).toString()//.parsedSafe<Response>()?.url?.substringBefore("""\""")?.trim()
-                    }
-            asyncTask.await()
+        val domain = "https://ser3.crazyblog.in"
+        println(link.substringAfterLast("/"))
+        val html = app.get("$domain/${link.substringAfterLast("/")}")
+        val cookies = html.headers.filter { it.first.contains("set-cookie") }.toString()
+        println(cookies)
+        val (AppSession, csrfToken, app_visitor) = cookies.split("),").map {
+            println("it: $it")
+            it.substringAfter("=").substringBefore("; path")
+        }
+        val document = html.document
+        val data = document.select("#go-link input")
+            .mapNotNull { it.attr("name").toString() to it.attr("value").toString() }.toMap()
+
+        return if (link.containsAnyOfIgnoreCase(apiSupportedLinks)) {
+            app.post(
+                url = apiUrl,
+                requestBody = json
+            ).parsedSafe<Response>()?.url
+        } else if (link.contains("ser2.crazyblog")) {
+            app.post(
+                url = "$domain/links/go",
+                headers = mapOf(
+                    "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8",
+                    "X-Requested-With" to "XMLHttpRequest",
+                    "Accept" to "application/json, text/javascript, */*; q=0.01",
+                    "Accept-Language" to "en-US,en;q=0.5",
+                ),
+                cookies = mapOf(
+                    "app_visitor" to app_visitor,
+                    "AppSession" to AppSession,
+                    "csrfToken" to csrfToken,
+
+                    ),
+                data =  data,
+                referer = "$domain/${link.substringAfterLast("/")}"
+            ).toString()
+        } else {
+            ""
         }
     }
 
-    private suspend fun bypassOlaRedirect(link: String): String {
+    private suspend fun bypassOlaRedirect(link: String, referer: String): String {
         //Log.d("myfirstlink", link)
-        val key = link.substringAfter("?key=").substringBefore("&id=")//.replace("%2B","+").replace("%3D","=").replace("%2F","/")
+        val key = link.substringAfter("?key=").substringBefore("&id=").replace("%2B","+").replace("%3D","=").replace("%2F","/")
         //Log.d("mykey", key)
         val id = link.substringAfter("&id=")
         //Log.d("myid", id)
         val param = mapOf(key to id)
-        val doc = app.get(link, referer = "$mainUrl/", params = param).document
+        val doc = app.get(link, referer = referer, params = param).document
         //Log.d("mydoctest", doc.toString())
         //Log.d("mydoc", doc.select("#download > a").attr("href"))
         return doc.select("#download > a").attr("href").trim()
+    }
+
+    val gdbot = "https://gdbot.xyz"
+
+    suspend fun extractGdbot(url: String): String? {
+        val headers = mapOf(
+            "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        )
+        val res = app.get(
+            "$gdbot/", headers = headers
+        )
+        val token = res.document.selectFirst("input[name=_token]")?.attr("value")
+        val cookiesSet = res.headers.filter { it.first == "set-cookie" }
+        val xsrf =
+            cookiesSet.find { it.second.contains("XSRF-TOKEN") }?.second?.substringAfter("XSRF-TOKEN=")
+                ?.substringBefore(";")
+        val session =
+            cookiesSet.find { it.second.contains("gdtot_proxy_session") }?.second?.substringAfter("gdtot_proxy_session=")
+                ?.substringBefore(";")
+
+        val cookies = mapOf(
+            "gdtot_proxy_session" to "$session",
+            "XSRF-TOKEN" to "$xsrf"
+        )
+        val requestFile = app.post(
+            "$gdbot/file", data = mapOf(
+                "link" to url,
+                "_token" to "$token"
+            ), headers = headers, referer = "$gdbot/", cookies = cookies
+        ).document
+
+        return requestFile.selectFirst("div.mt-8 a.float-right")?.attr("href")
+    }
+
+    fun getBaseUrl(url: String): String {
+        return URI(url).let {
+            "${it.scheme}://${it.host}"
+        }
+    }
+
+    data class DriveBotLink(
+        @JsonProperty("url") val url: String? = null,
+    )
+
+    suspend fun extractDrivebot(url: String): String? {
+        val iframeGdbot =
+            app.get(url).document.selectFirst("li.flex.flex-col.py-6 a:contains(Drivebot)")
+                ?.attr("href")
+        val driveDoc = app.get(iframeGdbot ?: return null)
+
+        val ssid = driveDoc.cookies["PHPSESSID"]
+        val script = driveDoc.document.selectFirst("script:containsData(var formData)")?.data()
+
+        val baseUrl = getBaseUrl(iframeGdbot)
+        val token = script?.substringAfter("'token', '")?.substringBefore("');")
+        val link =
+            script?.substringAfter("fetch('")?.substringBefore("',").let { "$baseUrl$it" }
+
+        val body = FormBody.Builder()
+            .addEncoded("token", "$token")
+            .build()
+        val cookies = mapOf("PHPSESSID" to "$ssid")
+
+        val result = app.post(
+            link,
+            requestBody = body,
+            headers = mapOf(
+                "Accept" to "*/*",
+                "Origin" to baseUrl,
+                "Sec-Fetch-Site" to "same-origin"
+            ),
+            cookies = cookies,
+            referer = iframeGdbot
+        ).text
+        return AppUtils.tryParseJson<DriveBotLink>(result)?.url
     }
 
     override suspend fun loadLinks(
@@ -268,45 +392,54 @@ class OlaMoviesProvider : MainAPI() { // all providers must be an instance of Ma
         callback: (ExtractorLink) -> Unit
     ): Boolean {
         // Log.d("mydata", data)
+        val me = parseJson<MovieLinks>(data)
+        //Log.d("mydata", me.list.toString())
+        me.list.forEach {
+            if (it.quality.isNotEmpty()) {
+                //Log.d("mydata", it.toString())
+                var count = 0
+                Log.d("mybypassolalinks1", bypassOlaRedirect(it.link, me.url))
+                Log.d("mydata", "got-here")
+                val shortLink = bypassOlaRedirect(it.link, me.url)
+                if (bypassOlaRedirect(it.link, me.url).isNotEmpty()) {
+                    while (count <= 10) {
+                        Log.d("mylist", "got")
+                        Log.d("myfinallinks1", extractDrivebot(extractGdbot(bypassAdLinks(shortLink).toString()).toString()).toString())
+                        count = count + 1
+                        safeApiCall {
+                            callback.invoke(
+                                ExtractorLink(
+                                    it.quality,
+                                    it.quality,
+                                    extractDrivebot(extractGdbot(bypassAdLinks(shortLink).toString()).toString()).toString(),
+                                    "$mainUrl/",
+                                    getQualityFromName(
+                                        Regex("(?i)((DVDRip)|(HD)|(HQ)|(HDRip))").find(it.quality)?.value.toString()
+                                            .lowercase()
+                                    ),
+                                    false
+                                )
+                            )
+                        }
+                    }
+                }
 
-        if (data.contains("[\"")) {
-            val firstLink = data.removePrefix("[\"").removeSuffix("\"]").split("\",\"").map { it.trim() }
-            // Log.d("myfirstlinks1", firstLink.toString())
-            val fixedfirstLink = arrayListOf<String>()
-            firstLink.filterTo(fixedfirstLink, { it != "https://olamovies.cyou<?>" })
-            fixedfirstLink.forEach {
-                Log.d("mybypassolalinks1", bypassOlaRedirect(it.substringBefore("<?>")))
-                Log.d("mybypasslinks1", bypassAdLinks(bypassOlaRedirect(it.substringBefore("<?>"))).toString())
-                //Log.d("mylist", mylinks.toString())
+            }/* else {
+                Log.d("mybypassolalinks1", bypassOlaRedirect(it.link, me.url))
+                Log.d("mybypasslinks2", bypassAdLinks(bypassOlaRedirect(it.link, me.url)).toString())
                 safeApiCall {
                     callback.invoke(
                         ExtractorLink(
-                            it.substringAfter("<?>"),
-                            it.substringAfter("<?>"),
-                            it.substringBefore("<?>"),
+                            "it.quality",
+                            "it.quality",
+                            data,
                             "$mainUrl/",
-                            getQualityFromName(Regex("(?i)((DVDRip)|(HD)|(HQ)|(HDRip))").find(it.substringAfter("<?>"))?.value.toString().lowercase()),
+                            Qualities.Unknown.value,
                             false
                         )
                     )
                 }
-            }
-        } else {
-            val firstLink = data
-            Log.d("mybypassolalinks1", bypassOlaRedirect(firstLink.substringBefore("<?>")))
-            Log.d("mybypasslinks2", bypassAdLinks(bypassOlaRedirect(firstLink.substringBefore("<?>"))).toString())
-            safeApiCall {
-                callback.invoke(
-                    ExtractorLink(
-                        firstLink.substringAfter("<?>"),
-                        firstLink.substringAfter("<?>"),
-                        firstLink.substringBefore("<?>"),
-                        "$mainUrl/",
-                        getQualityFromName(Regex("(?i)((DVDRip)|(HD)|(HQ)|(HDRip))").find(firstLink.substringAfter("<?>"))?.value.toString().lowercase()),
-                        false
-                    )
-                )
-            }
+            }*/
         }
         return true
     }
