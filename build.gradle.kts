43d0a296b074475eb8f26a407fa650d2999356b5
LagradOst <46196380+Blatzar@users.noreply.github.com>
2023-01-11 09:34:09 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,18 +44,18 @@ subprojects {
         compileSdkVersion(33)
 
         defaultConfig {
-            minSdk = 26
+            minSdk = 21
             targetSdk = 33
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
         }
 
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
             kotlinOptions {
-                jvmTarget = "11" // Required
+                jvmTarget = "1.8" // Required
                 // Disables some unnecessary features
                 freeCompilerArgs = freeCompilerArgs +
                         "-Xno-call-assertions" +
