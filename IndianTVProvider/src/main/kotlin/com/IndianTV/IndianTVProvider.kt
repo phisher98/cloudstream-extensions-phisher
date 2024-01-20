78d93a5ddd68b4629a700631ab82499d88412aa6
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-01-20 18:43:54 +0530

    update

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
@@ -5,8 +5,8 @@ import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 
 class IndianTVProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
+    override var mainUrl = "https://livesportsclub.me/hls/tata/" 
+    override var name = "IndianTV"
     override val supportedTypes = setOf(TvType.Movie)
 
     override var lang = "hi"
