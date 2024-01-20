f1b299473fe99c9fd3113da95b2f9129a6346f97
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-01-20 19:30:42 +0530

    update

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
@@ -26,20 +26,8 @@ class IndianTVProvider : MainAPI() { // all providers must be an instance of Mai
         request: MainPageRequest
     ): HomePageResponse {
         val document = app.get(request.data).document
-        
-    //Log.d("Document", request.data)
 
-        val home = if (request.data.contains("Search")) {
-            document.select("div.col-span-4").mapNotNull {
-                it.toEpicSearchResult()
-            }
-        } else {
-            document.select("div.col-6").mapNotNull {
-                it.toSearchResult()
-            }
-        }
-
-        return HomePageResponse(arrayListOf(HomePageList(request.name, home, isHorizontalImages = true)), hasNext = true)
+        return HomePageResponse(arrayListOf(HomePageList(request.name, document, isHorizontalImages = true)), hasNext = true)
     }
 
     // this function gets called when you search for something
@@ -48,53 +36,8 @@ class IndianTVProvider : MainAPI() { // all providers must be an instance of Mai
     }
 
         override suspend fun load(url: String): LoadResponse {
-        val data = parseJson<LiveStreamLinks>(url)
         val title = data.title
         val poster = data.poster
         val link = data.link
 
-private fun Element.toSearchResult(): SearchResponse {
-        //Log.d("Got","got here")
-        val titleRaw = this.selectFirst("div.card > a > div.info > span")?.text()?.trim()
-        val title = if (titleRaw.isNullOrBlank()) "Unknown LiveStream" else titleRaw.toString()
-        //Log.d("title", title)
-        val posterUrl = fixUrl(this.select("img").attr("src"))
-        //Log.d("posterUrl", posterUrl)
-        val href = fixUrl(this.selectFirst("a")?.attr("href").toString())
-        //Log.d("mybadhref", href)
-        val loadData = LiveStreamLinks(
-                title,
-                posterUrl,
-                href
-            ).toJson()
-       return newMovieSearchResponse(title, loadData, TvType.Live) {
-                this.posterUrl = posterUrl
-            }
-    }
-        
-
-private fun Element.toEpicSearchResult(): SearchResponse {
-        //Log.d("Got","got here")
-        val titleRaw = this.selectFirst("div.livetv > h3 > span")?.text()?.trim()
-        val title = if (titleRaw.isNullOrBlank()) "Unknown LiveStream" else titleRaw.toString()
-        //Log.d("title", title)
-        val posterUrl = fixUrl(this.select("div.livetc-right > img").attr("src"))
-        //Log.d("posterUrl", posterUrl)
-        val href = fixUrl(this.selectFirst("div.livetv > a")?.attr("href").toString())
-        //Log.d("href", href)
-        val loadData = LiveStreamLinks(
-            title,
-            posterUrl,
-            href
-        ).toJson()
-
-        return newMovieSearchResponse(title, loadData, TvType.Live) {
-            this.posterUrl = posterUrl
-        }
-    }
-
-        return newMovieLoadResponse(title, link, TvType.Live, link) {
-                this.posterUrl = poster
-            }
-        }
 }
