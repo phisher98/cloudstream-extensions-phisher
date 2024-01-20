7df7c7b7008782b79dd09edd439f50e4a35b7fdc
CloudExt <me@mydomain.com>
2024-01-20 18:28:59 +0530

    Add Providers

--- IndianTVProvider/build.gradle.kts
+++ IndianTVProvider/build.gradle.kts
@@ -1,24 +1,24 @@
 // use an integer for version numbers
-version = 5
+version = 1
 
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    // description = "Lorem Ipsum"
-    authors = listOf("Adippe")
+    description = "Lorem Ipsum"
+    authors = listOf("Cloudburst")
 
     /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     * */
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
     status = 1 // will be 3 if unspecified
-    tvTypes = listOf(
-        "Live",
-    )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=github.com&sz=%size%"
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of avaliable types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf("Others")
 }
