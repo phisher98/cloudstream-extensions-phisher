115fcb920604c850d0303b80e3a120dd19093d31
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-01-20 18:49:04 +0530

    update

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
@@ -7,12 +7,19 @@ import com.lagradost.cloudstream3.SearchResponse
 class IndianTVProvider : MainAPI() { // all providers must be an instance of MainAPI
     override var mainUrl = "https://livesportsclub.me/hls/tata/" 
     override var name = "IndianTV"
-    override val supportedTypes = setOf(TvType.Movie)
+    override val supportedTypes = setOf(TvType.Live)
 
     override var lang = "hi"
 
     // enable this when your provider has a main page
     override val hasMainPage = true
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        val document = app.get(request.data).document
+    }
 
     // this function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
