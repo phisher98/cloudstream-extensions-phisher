57575e75909b450c857d812e8203d5720ab474c9
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-01-20 18:37:17 +0530

    update

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class TestPlugin: Plugin() {
+class IndianTVPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(IndianTVProvider())
