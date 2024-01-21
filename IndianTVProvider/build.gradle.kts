c6f6daa064aca8c2a12aa973891e8fce49462294
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-01-21 15:51:02 +0530

    Update build.gradle.kts

--- IndianTVProvider/build.gradle.kts
+++ IndianTVProvider/build.gradle.kts
@@ -15,7 +15,7 @@ cloudstream {
     * 2: Slow
     * 3: Beta only
     * */
-    status = 1 // will be 3 if unspecified
+    status = 0 // will be 3 if unspecified
 
     // List of video source types. Users are able to filter for extensions in a given category.
     // You can find a list of avaliable types here:
