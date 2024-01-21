293988e9654501fc60796332dc2f3a6436b95d0b
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-01-21 12:50:15 +0530

    update

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
@@ -2,16 +2,11 @@ package com.IndianTV
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.SearchResponse
-import com.fasterxml.jackson.annotation.JsonProperty
-import org.jsoup.nodes.Element
-import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.extractors.JWPlayer
-import com.lagradost.cloudstream3.mvvm.safeApiCall
-import com.lagradost.nicehttp.NiceResponse
+import org.jsoup.nodes.Element
 
-class IndianTVProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://livesportsclub.me" 
+class IndianTVProvider : MainAPI() {
+    override var mainUrl = "https://livesportsclub.me"
     override var name = "IndianTV"
     override val supportedTypes = setOf(TvType.Live)
     override var lang = "hi"
@@ -31,18 +26,19 @@ class IndianTVProvider : MainAPI() { // all providers must be an instance of Mai
         }
         return newHomePageResponse(request.name, home)
     }
+
     private fun Element.toSearchResult(): SearchResponse {
-        val href = this.selectFirst("a")!!.attr("href")
-        val titleRaw = this.selectFirst("h2.text-center text-sm font-bold")?.text()?.trim()
+        val href = fixUrl(this.select("a").attr("href"))
+        val titleRaw = this.selectFirst("h2.text-center.text-sm.font-bold")?.text()?.trim()
         val title = if (titleRaw.isNullOrBlank()) "Unknown LiveStream" else titleRaw.toString()
         val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
-    return newMovieSearchResponse(title, href, TvType.Live) {
-        this.posterUrl = posterUrl
-    }
-}
 
-    // this function gets called when you search for something
+        return newMovieSearchResponse(title, href, TvType.Live) {
+            this.posterUrl = posterUrl
+        }
+    }
+
     override suspend fun search(query: String): List<SearchResponse> {
-        return listOf<SearchResponse>()
+        return listOf()
     }
 }
