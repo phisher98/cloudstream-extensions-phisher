60a16a9979d9ddc9ecd7885147d6ffdba1dca645
CloudExt <me@mydomain.com>
2024-01-21 17:55:21 +0530

    add Providers

---
+++ TATATVProvider/build.gradle.kts
@@ -0,0 +1,24 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Indian Live TV TATA"
+    authors = listOf("HindiProvider")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Live",
+    )
+
+    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/5/52/JioTV_logo.png"
+}
