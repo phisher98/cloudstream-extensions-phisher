b9a714a228ab1680e6fb05b8de7e27df628deca2
CloudExt <me@mydomain.com>
2024-01-24 12:27:30 +0530

    add Providers

---
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
@@ -0,0 +1,45 @@
+package com.IndianTV
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.utils.*
+import org.jsoup.nodes.Element
+import java.net.URL
+
+class IndianTVProvider : MainAPI() {
+    override var mainUrl = "https://livesportsclub.me"
+    override var name = "IndianTV"
+    override val supportedTypes = setOf(TvType.Live)
+    override var lang = "hi"
+    override val hasMainPage = true
+
+    override val mainPage = mainPageOf(
+        "$mainUrl/hls/tata/" to "Tata",
+    )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest
+	): HomePageResponse {
+         val document = app.get(request.data + page).document
+        val home = document.select("div.venz > ul > li").mapNotNull {
+            it.toSearchResult()
+        }
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun Element.toSearchResult(): SearchResponse {
+        val href = resolveUrl(this.select("a").attr("href"))
+        val title = this.selectFirst("h2.text-center.text-sm.font-bold")?.text()?.trim() ?: "Unknown Title"
+        val posterUrl = resolveUrl(this.selectFirst("img")?.attr("src"))
+        return newMovieSearchResponse(title, href, TvType.Live) {
+            this.posterUrl = posterUrl
+        }
+    }
+
+    private fun resolveUrl(url: String?): String {
+        return URL(URL(mainUrl), url).toString()
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        return emptyList() // Modify this if you implement search functionality
+    }
+}
