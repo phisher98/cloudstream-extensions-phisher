2c4cb14f5f6e9b5d5589414ec80a789fe7686848
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-02-20 21:34:26 +0530

    update

--- spankbang/src/main/kotlin/com/Spankbang/spankbang.kt
+++ spankbang/src/main/kotlin/com/Spankbang/spankbang.kt
@@ -85,12 +85,12 @@ class spankbang : MainAPI() {
     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
         val document = app.get(data).document
 
-        document.select("div.video_container").map { res ->
+        document.select("div#video_container").map { res ->
             callback.invoke(
                     ExtractorLink(
                         source  = this.name,
                         name    = this.name,
-                        url     = fixUrl(res.selectFirst("video.vjs-tech")?.attr("src")?.trim().toString()),
+                        url     = fixUrl(res.selectFirst("video.vjs-tech")?.attr("src")),
                         referer = data,
                         quality = Qualities.Unknown.value
                     )
