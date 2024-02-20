ec05567c153bf57f6371c5564e345ceb03ae83ad
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-02-20 22:23:12 +0530

    update

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
@@ -1,13 +1,92 @@
 package com.IndianTV
 
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
 
-@CloudstreamPlugin
-class IndianTVPlugin: Plugin() {
-    override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(IndianTVProvider())
+class IndianTV : MainAPI() {
+    override var mainUrl              = "https://madplay.live/hls/tata/"
+    override var name                 = "IndianTV"
+    override val hasMainPage          = true
+    override var lang                 = "hi"
+    override val hasQuickSearch       = false
+    override val hasDownloadSupport   = true
+    override val hasChromecastSupport = true
+    override val supportedTypes       = setOf(TvType.Live)
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val document = app.get(request.data + page).document
+        val home     = document.select("div.box1").mapNotNull { it.toSearchResult() }
+
+        return newHomePageResponse(
+            list    = HomePageList(
+                name               = request.name,
+                list               = home,
+                isHorizontalImages = false
+            ),
+            hasNext = true
+        )
+    }
+
+    private fun Element.toSearchResult(): SearchResponse {
+        val title     = fixTitle(this.selectFirst("h2.text-center.text-sm.font-bold"))?.text()?.trim().toString()
+        val href      = fixUrl(this.select("[target=_blank]").attr("href"))
+        val posterUrl = fixUrlNull(this.select("img").attr("src"))
+
+        return newMovieSearchResponse(title, href, TvType.Movie) {
+            this.posterUrl = posterUrl
+        }
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val searchResponse = mutableListOf<SearchResponse>()
+
+        for (i in 1..10) {
+            val document = app.get("${mainUrl}/page/$i?s=$query").document
+
+            val results = document.select("div.video-item").mapNotNull { it.toSearchResult() }
+
+            if (!searchResponse.containsAll(results)) {
+                searchResponse.addAll(results)
+            } else {
+                break
+            }
+
+            if (results.isEmpty()) break
+        }
+
+        return searchResponse
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url).document
+
+        val title       = document.selectFirst("meta[property=og:title]")?.attr("content")?.trim().toString()
+        val poster      = fixUrlNull(document.selectFirst("meta[property='og:image']")?.attr("content"))
+        val description = document.selectFirst("meta[property=og:description]")?.attr("content")?.trim()
+    
+
+        return newMovieLoadResponse(title, url, TvType.NSFW, url) {
+            this.posterUrl = poster
+            this.plot      = description
+        }
+    }
+
+    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
+        val document = app.get(data).document
+
+        document.select("video.vjs-tech").map { res ->
+            callback.invoke(
+                    ExtractorLink(
+                        source  = this.name,
+                        name    = this.name,
+                        url     = fixUrl(res.attr("src")?.trim().toString()),
+                        referer = data,
+                        quality = Qualities.Unknown.value
+                    )
+            )
+        }
+
+        return true
     }
 }
--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
@@ -1,73 +1,16 @@
 package com.IndianTV
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.utils.AppUtils.parseJson
-import com.lagradost.cloudstream3.utils.AppUtils.toJson
-import com.lagradost.cloudstream3.mvvm.safeApiCall
-import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
-
-class IndianTVProvider : MainAPI() {
-    override var mainUrl = "https://madplay.live/hls/tata"
-    override var name = "IndianTV"
-    override val supportedTypes = setOf(TvType.Live,
-    )
-    override var lang = "hi"
-    override val hasMainPage = true
-
-    data class LiveStreamLinks (
-        @JsonProperty("title")  val title: String,
-        @JsonProperty("poster") val poster: String,
-        @JsonProperty("link")   val link: String,
-    )
-
-    override suspend fun getMainPage(page: Int, request: MainPageRequest
-	): HomePageResponse {
-        val document = app.get(request.data).document
-        val home = document.select("div#listContainer").mapNotNull {
-            it.toSearchResult()
-        }
-        return HomePageResponse(arrayListOf(HomePageList(request.name, home, isHorizontalImages = false)), hasNext = false)
+@CloudstreamPlugin
+class IndianTVProvider: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(IndianTV())
     }
-
-    private fun Element.toSearchResult(): SearchResponse {
-        val titleRaw = this.selectFirst("h2.text-center.text-sm.font-bold")?.text()?.trim()
-        val title = if (titleRaw.isNullOrBlank()) "Unknown LiveStream" else titleRaw.toString()
-        //Log.d("title", title)
-        val posterUrl = fixUrl(this.select("img").attr("src"))
-        //Log.d("posterUrl", posterUrl)
-        val href = fixUrl(this.selectFirst("a")?.attr("href").toString())
-        //Log.d("mybadhref", href)
-        val loadData = LiveStreamLinks(
-                title,
-                posterUrl,
-                href
-            ).toJson()
-        return newMovieSearchResponse(title, loadData, TvType.Live) {
-                this.posterUrl = posterUrl
-            }
-        }
-
-    
-        /*override suspend fun search(query: String): List<SearchResponse> {
-            val doc = app.get("$mainUrl/").document
-            //Log.d("document", document.toString())
-    
-            return doc.select("div.col-6").mapNotNull {
-                it.toSearchResult()
-            }.filter { it.name.contains(query, true) }
-        }*/
-
-        override suspend fun load(url: String): LoadResponse {
-            val data = parseJson<LiveStreamLinks>(url)
-            val title = data.title
-            val poster = data.poster
-            val link = data.link
-
-            return newMovieLoadResponse(title, link, TvType.Live, link) {
-                this.posterUrl = poster
-            }
-        }
 }
+
+
+
