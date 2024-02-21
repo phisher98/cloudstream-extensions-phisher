ab9b533f16e30fc476776c62a5278d8daa17e3a3
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-02-21 23:25:31 +0530

    update

--- mydesi/src/main/kotlin/com/mydesi/mydesi.kt
+++ mydesi/src/main/kotlin/com/mydesi/mydesi.kt
@@ -4,32 +4,27 @@ import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 
-class spankbang : MainAPI() {
-    override var mainUrl              = "https://spankbang.com"
-    override var name                 = "Spankbang"
+class mydesi : MainAPI() {
+    override var mainUrl              = "https://www.mydesi2.net"
+    override var name                 = "Mydesi"
     override val hasMainPage          = true
-    override var lang                 = "en"
+    override var lang                 = "hi"
     override val hasQuickSearch       = false
     override val hasDownloadSupport   = true
     override val hasChromecastSupport = true
     override val supportedTypes       = setOf(TvType.NSFW)
 
     override val mainPage = mainPageOf(
-            "${mainUrl}/trending_videos/" to "New Videos",
-            "${mainUrl}/j2/channel/familyxxx/" to "Family XXX",
-            "${mainUrl}/ce/channel/bratty+milf/" to "Bratty MILF",
-            "${mainUrl}/cf/channel/bratty+sis/" to "Bratty Sis",
-            "${mainUrl}/k5/channel/japan+hdv/" to "Japan HDV",
-            "${mainUrl}/j3/channel/hot+wife+xxx/" to "Hot Wife XXX",
-            "${mainUrl}/d6/channel/my+family+pies/" to "My Family Pies",
-            "${mainUrl}/ho/channel/brazzers/" to "Brazzers",
-            "${mainUrl}/6l/channel/mylf/" to "MYLF",
-            "${mainUrl}/9n/channel/evil+angel/" to "Evil Angel",
+            "${mainUrl}/page/" to "New Videos",
+            "${mainUrl}/category/tango/" to "Tango",
+            "${mainUrl}/category/pornography/onlyfans-video-collection/" to "OnlyFans",
+            "${mainUrl}/category/paid/" to "Webseries",
+            "${mainUrl}/category/village/" to "Village",
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get(request.data + page).document      
-        val home     = document.select("div.video-list-with-ads > div.video-item").mapNotNull { it.toSearchResult() }
+        val home     = document.select("div.col-12 > div.row.no-gutters").mapNotNull { it.toSearchResult() }
 
         return newHomePageResponse(
             list    = HomePageList(
@@ -43,8 +38,8 @@ class spankbang : MainAPI() {
 
     private fun Element.toSearchResult(): SearchResponse {
         val title     = fixTitle(this.select("a.thumb > picture > img").attr("alt")).trim().toString()
-        val href      = fixUrl(this.select("a.thumb").attr("href"))
-        val posterUrl = fixUrlNull(this.select("a.thumb > picture > img").attr("data-src"))
+        val href      = fixUrl(this.select("a.infos").attr("href"))
+        val posterUrl = fixUrlNull(this.select("a.thumb > img").attr("data-src"))
 
         return newMovieSearchResponse(title, href, TvType.Movie) {
             this.posterUrl = posterUrl
@@ -57,7 +52,7 @@ class spankbang : MainAPI() {
         for (i in 1..10) {
             val document = app.get("${mainUrl}/page/$i?s=$query").document
 
-            val results = document.select("div.video-list-with-ads > div.video-item").mapNotNull { it.toSearchResult() }
+            val results = document.select("div.col-12 > div.row.no-gutters").mapNotNull { it.toSearchResult() }
 
             if (!searchResponse.containsAll(results)) {
                 searchResponse.addAll(results)
@@ -75,7 +70,7 @@ class spankbang : MainAPI() {
         val document = app.get(url).document
 
         val title       = document.selectFirst("meta[property=og:title]")?.attr("content")?.trim().toString()
-        val poster      = fixUrlNull(document.selectFirst("meta[property='og:image']")?.attr("content"))
+        val poster      = fixUrlNull(document.selectFirst("#censor-player_html5_api")?.attr("poster"))
         val description = document.selectFirst("meta[property=og:description]")?.attr("content")?.trim()
     
 
@@ -88,12 +83,12 @@ class spankbang : MainAPI() {
     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
         val document = app.get(data).document
 
-        document.select("div#video_container").map { res ->
+        document.select("#censor-player_html5_api").map { res ->
             callback.invoke(
                     ExtractorLink(
                         source  = this.name,
                         name    = this.name,
-                        url     = fixUrl(res.selectFirst("video > source")?.attr("src")?.trim().toString()),
+                        url     = fixUrl(res.selectFirst("source")?.attr("src")?.trim().toString()),
                         referer = data,
                         quality = Qualities.Unknown.value
                     )
--- mydesi/src/main/kotlin/com/mydesi/mydesiProvider.kt
+++ mydesi/src/main/kotlin/com/mydesi/mydesiProvider.kt
@@ -5,8 +5,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class spankbangProvider: Plugin() {
+class mydesiProvider: Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(spankbang())
+        registerMainAPI(mydesi())
     }
 }
