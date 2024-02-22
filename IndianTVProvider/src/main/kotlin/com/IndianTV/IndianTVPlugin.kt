b13e80445a5096498b5aac504882560c70881e4a
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-02-22 15:44:03 +0530

    update

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
@@ -14,7 +14,7 @@ class IndianTVPlugin : MainAPI() {
     override var name                 = "TATA Sky"
     override val hasMainPage          = true
     override var lang                 = "hi"
-    override val hasQuickSearch       = false
+    override val hasQuickSearch       = true
     override val hasDownloadSupport   = true
     override val hasChromecastSupport = true
     override val supportedTypes       = setOf(TvType.Live)
@@ -25,15 +25,7 @@ class IndianTVPlugin : MainAPI() {
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get(request.data + page).document
-        val home     = 
-        if (request.data.contains("tata")){
-            document.select("div#listContainer").mapNotNull { it.toSearchResult() }
-        }
-        else
-        {
-            document.select("div#listContainer").mapNotNull { it.toSearchResult()
-        }
-    }
+        val home     = document.select("div#listContainer > div").mapNotNull { it.toSearchResult()}
 
         return newHomePageResponse(
             list    = HomePageList(
@@ -46,9 +38,9 @@ class IndianTVPlugin : MainAPI() {
     }
 
     private fun Element.toSearchResult(): SearchResponse {
-        val title     = this.select("div.box1 > a > div > div > h2").text()
-        val href      = fixUrl(this.select("div.box1> a").attr("href"))
-        val posterUrl = fixUrlNull(this.select("div.box1 > a > div > img").attr("src"))
+        val title     = this.select("h2.text-center").text()
+        val href      = fixUrl(this.select("a").attr("href"))
+        val posterUrl = fixUrlNull(this.select("img").attr("src"))
 
         return newMovieSearchResponse(title, href, TvType.Live) {
             this.posterUrl = posterUrl
@@ -57,7 +49,7 @@ class IndianTVPlugin : MainAPI() {
 
     override suspend fun search(query: String): List<SearchResponse> {
         val searchResponse = mutableListOf<SearchResponse>()
-
+/* 
         for (i in 1..10) {
             val document = app.get("${mainUrl}/page/$i?s=$query").document
 
@@ -73,6 +65,7 @@ class IndianTVPlugin : MainAPI() {
         }
 
         return searchResponse
+        */
     }
 
     override suspend fun load(url: String): LoadResponse {
