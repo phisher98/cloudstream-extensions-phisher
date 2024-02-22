55b203980f8f126788d385434f822de7b61f5be0
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-02-22 23:59:41 +0530

    update fix

--- MasaFun/src/main/kotlin/com/Masafun/MasaFunProvider.kt
+++ MasaFun/src/main/kotlin/com/Masafun/MasaFunProvider.kt
@@ -5,8 +5,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class masaFunProvider: Plugin() {
+class MasaFunProvider: Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(masafun())
+        registerMainAPI(masaFun())
     }
 }
