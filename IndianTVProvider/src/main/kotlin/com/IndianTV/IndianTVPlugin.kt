9a0278258161b95a9b449d5885855550e95f0a14
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-02-23 15:33:28 +0530

    update

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
@@ -1,8 +1,16 @@
+package com.coxju
+
 import org.jsoup.nodes.Element
+import org.jsoup.*
 import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import java.util.*
+
+
+public val homePoster ="https://raw.githubusercontent.com/phisher98/HindiProviders/master/TATATVProvider/src/main/kotlin/com/lagradost/0-compressed-daf4.jpg"
 
 class IndianTVPlugin : MainAPI() {
-    override var mainUrl              = "https://madplay.live/hls/tata/"
+    override var mainUrl              = "https://madplay.live/hls/tata"
     override var name                 = "TATA Sky"
     override val hasMainPage          = true
     override var lang                 = "hi"
@@ -11,25 +19,20 @@ class IndianTVPlugin : MainAPI() {
     override val hasChromecastSupport = true
     override val supportedTypes       = setOf(TvType.Live)
 
-      override val mainPage = mainPageOf(
+   override val mainPage = mainPageOf(
         "${mainUrl}/" to "TATA",
-    )
+)
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get(request.data).document
-        val home     = document.select("div#listContainer > div.box1").mapNotNull { it.toSearchResult() }
-        val groupedHome = home.groupBy { it.category } // Grouping by category
-
-        val homePageLists = groupedHome.map { (category, items) ->
-            HomePageList(
-                name = category ?: request.name, // Using category as name if available
-                list = items,
-                isHorizontalImages = true
-            )
-        }
+        val home     = document.select("div#listContainer > div.box1").mapNotNull { it.toSearchResult()}
 
         return newHomePageResponse(
-            list    = homePageLists,
+            list    = HomePageList(
+                name               = request.name,
+                list               = home,
+                isHorizontalImages = true
+            ),
             hasNext = false
         )
     }
@@ -38,20 +41,15 @@ class IndianTVPlugin : MainAPI() {
         val title     = this.select("h2.text-center").text()
         val href      = fixUrl(this.select("a").attr("href"))
         val posterUrl = fixUrlNull(this.select("img").attr("src"))
-        val category  = this.select("p").text()
+        //val category = this.select("p").text()
 
         return newMovieSearchResponse(title, href, TvType.Live) {
             this.posterUrl = posterUrl
-            this.category = category // Storing category
         }
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
         val searchResponse = mutableListOf<SearchResponse>()
-        /* 
-        for (i in 1..10) {
-            val document = app.get("${mainUrl}/page/$i?s=$query").document
-
             val results = document.select("div.box1").mapNotNull { it.toSearchResult() }
 
             if (!searchResponse.containsAll(results)) {
@@ -61,9 +59,9 @@ class IndianTVPlugin : MainAPI() {
             }
 
             if (results.isEmpty()) break
-        }
-        */
+        
         return searchResponse
+        
     }
 
     override suspend fun load(url: String): LoadResponse {
@@ -71,8 +69,9 @@ class IndianTVPlugin : MainAPI() {
 
         val title       = document.selectFirst("div.program-info > span.channel-name")?.text()?.trim().toString()
         val poster      = fixUrl("https://raw.githubusercontent.com/phisher98/HindiProviders/master/TATATVProvider/src/main/kotlin/com/lagradost/0-compressed-daf4.jpg")
-        val showname    = document.selectFirst("div.program-info > div.program-name")?.text()?.trim().toString()
+        var showname = document.selectFirst("div.program-info > div.program-name")?.text()?.trim().toString()
         val description = document.selectFirst("div.program-info > div.program-description")?.text()?.trim().toString()
+    
 
         return newMovieLoadResponse(title, url, TvType.Live, url) {
             this.posterUrl = poster
@@ -81,20 +80,46 @@ class IndianTVPlugin : MainAPI() {
     }
     
     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
-        val document = app.get(data).document
+        //val document = app.get(data).document
+        val linksData = AppUtils.parseJson<Links>(data)
+        //Log.d("King", "videoUrl:$mainUrl/play.php?id=${linksData.id}")
+        val document = app.get(
+            url = "$mainUrl/play.php?id=${linksData.id}",
+            headers = mapOf(
+                "user-agent" to userAgent
+            )).document
 
-        document.select("video.vjs-tech").map { res ->
-            callback.invoke(
-                    ExtractorLink(
-                        source  = this.name,
-                        name    = this.name,
-                        url     = fixUrl(res.attr("src")?.trim().toString()),
-                        referer = data,
-                        quality = Qualities.Unknown.value
-                    )
-            )
+        val servers = document.select("ul[id=\"xservers\"]").select("button").mapNotNull {
+            it.attr("data-embed")
         }
+        //Log.d("King", "servers:$servers")
 
+        servers.map {
+            app.get(it).document.select("script").mapNotNull { script ->
+
+                val finalScript = if (JsUnpacker(script.data()).detect()) {
+                    JsUnpacker(script.data()).unpack()!!
+                } else {
+                    script.data()
+                }
+
+                if (finalScript.contains("sources:")) {
+                    val link = finalScript.substringAfter("sources:")
+                        .substringBefore("\"}]").replace("[{file:\"","")
+
+                    callback.invoke(
+                        ExtractorLink(
+                            source = it,
+                            name = URL(it).host,
+                            url = link,
+                            referer = "",
+                            quality = Qualities.Unknown.value,
+                            isM3u8 = false,
+                        )
+                    )
+                }
+            }
+        }
         return true
     }
 }
