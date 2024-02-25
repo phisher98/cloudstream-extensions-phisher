a665d6c707211704acc59d7b739d5cdef092ae60
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-02-25 16:38:10 +0530

    etts

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
@@ -101,14 +101,13 @@ class IndianTVPlugin : MainAPI() {
     
     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
         val document = app.get(data).document
-        Log.d("King3",scriptData)
+        Log.d("King3",document)
         val scripts = document.select("script")
         scripts.mapNotNull { script ->
         val scriptData = script.data()
         Log.d("King2",scriptData)
 
         if (scriptData.contains("split")){
-            Log.d("KingScriptHead1",scriptData)
             val javascriptResult = scriptData.runJS("result").split(",").toString()
                 Log.d("KingScriptHead",javascriptResult)
                     callback.invoke(
