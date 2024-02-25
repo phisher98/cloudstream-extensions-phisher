f7e7dd5870b4dedae7b8f27ada70f2c46e140825
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-02-25 19:24:20 +0530

    test 12

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import org.jsoup.nodes.Element
 import org.jsoup.*
 import com.lagradost.cloudstream3.*
-import dev.datlag.jsunpacker.JsUnpacker
 import com.lagradost.cloudstream3.utils.*
 import java.util.*
 import java.io.File
@@ -13,27 +12,6 @@ import org.json.JSONObject
 import com.lagradost.cloudstream3.extractors.JWPlayer
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
-
-fun String.runJS(variableName: String): String {
-    val rhino = Context.enter()
-    rhino.initSafeStandardObjects()
-    rhino.optimizationLevel = -1
-    val scope: Scriptable = rhino.initSafeStandardObjects()
-    val script = this
-    val result: String
-    try {
-        var js = ""
-        for (i in script.indices) {
-            js += script[i]
-        }
-        rhino.evaluateString(scope, js, "JavaScript", 1, null)
-        result = Context.toString(scope.get(variableName, scope))
-    } finally {
-        Context.exit()
-    }
-    Log.d("King",result)
-    return result
-}
 
 
 public val homePoster ="https://raw.githubusercontent.com/phisher98/HindiProviders/master/TATATVProvider/src/main/kotlin/com/lagradost/0-compressed-daf4.jpg"
@@ -107,7 +85,11 @@ class IndianTVPlugin : MainAPI() {
         Log.d("King2",scriptData)
 
         if (scriptData.contains("split")){
-            val finalScript = scriptData.runJS("result").split(",").toString()
+            val finalScript =(JsHunter(scriptData).detect()) {
+                Jshunter(scriptData).unpack().toString()
+            } else {
+                scriptData
+            }
             Log.d("KingScriptHead1",finalScript)
                     callback.invoke(
                     DrmExtractorLink(
