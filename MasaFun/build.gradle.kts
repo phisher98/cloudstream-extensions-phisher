825c09316c7befb009195a54520f925a6f4fe304
CloudExt <me@mydomain.com>
2024-02-28 15:45:48 +0530

    update

--- MasaFun/build.gradle.kts
+++ MasaFun/build.gradle.kts
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 1 // will be 3 if unspecified
+    status  = 0  // will be 3 if unspecified
     tvTypes = listOf("NSFW")
     iconUrl = "https://www.google.com/s2/favicons?domain=uncutmaza.cc&sz=%size%"
 }
