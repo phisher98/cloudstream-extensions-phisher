553880bd882bbae39517b7306eb74213568136b3
CloudExt <me@mydomain.com>
2024-02-28 09:39:13 +0530

    update

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVPlugin.kt
@@ -12,6 +12,13 @@ import org.mozilla.javascript.Scriptable
 import android.util.Base64
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.Coroutines.mainWork
+import com.lagradost.cloudstream3.utils.Coroutines.runOnMainThread
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.withContext
 import java.nio.charset.StandardCharsets
 
 fun hexStringToByteArray(hexString: String): ByteArray {
@@ -107,29 +114,32 @@ class IndianTVPlugin : MainAPI() {
         val document = app.get(data).document
         val scripts = document.select("script")
 
-        scripts.map { script ->
+        val scriptPromises = mutableListOf<Deferred<Unit>>()
+
+        scripts.forEach { script ->
             val finalScriptRaw = script.data().toString()
-            mainWork {
+
+            val scriptPromise = CoroutineScope(Dispatchers.Default).async {
                 if (finalScriptRaw.contains("split")) {
                     val startJs =
                         """
-        var globalArgument = null;
-        function jwplayer() {
-            return {
-                id: null,
-                setup: function(arg) {
-                    globalArgument = arg;
-                }
-            };
+    var globalArgument = null;
+    function jwplayer() {
+        return {
+            id: null,
+            setup: function(arg) {
+                globalArgument = arg;
+            }
         };
-        """
+    };
+    """
                     val rhino = getRhinoContext()
                     val scope: Scriptable = rhino.initSafeStandardObjects()
                     rhino.evaluateString(scope, startJs + finalScriptRaw, "JavaScript", 1, null)
                     val rhinout = scope.get("globalArgument", scope).toJson()
-                    Log.d("Rhinoout", rhinout)
 
-                    val pattern = """"file":"(.*?)".*?"keyId":"(.*?)".*?"key":"(.*?)"""".toRegex()
+                    val pattern =
+                        """"file":"(.*?)".*?"keyId":"(.*?)".*?"key":"(.*?)"""".toRegex()
                     val matchResult = pattern.find(rhinout)
                     var file: String? = null
                     var keyId: String? = null
@@ -152,27 +162,36 @@ class IndianTVPlugin : MainAPI() {
                     val finalkey = (byteArrayToBase64(byteArrakey))
                     Log.d("finalkey", "Base64 Encoded String: $finalkey")
 
-                    callback.invoke(
-                        DrmExtractorLink(
-                            source = "TATA",
-                            name = "TATA",
-                            url = link,
-                            referer = "madplay.live",
-                            quality = Qualities.Unknown.value,
-                            type = INFER_TYPE,
-                            kid = finalkeyid,
-                            key = finalkey,
-                        )
+                    // Construct the ExtractorLink object
+                    val extractorLink = DrmExtractorLink(
+                        source = "TATA",
+                        name = "TATA",
+                        url = link,
+                        referer = "madplay.live",
+                        quality = Qualities.Unknown.value,
+                        type = INFER_TYPE,
+                        kid = finalkeyid,
+                        key = finalkey,
                     )
-                    Log.d("DRM",finalkeyid)
-                    Log.d("DRM",finalkey)
+
+                    // Invoke the callback
+                    withContext(Dispatchers.Main) {
+                        callback.invoke(extractorLink)
                     }
                 }
+            }
 
+            scriptPromises.add(scriptPromise)
         }
+
+        // Wait for all script promises to complete
+        scriptPromises.awaitAll()
+
         return true
     }
+
 }
+
 
     
 
