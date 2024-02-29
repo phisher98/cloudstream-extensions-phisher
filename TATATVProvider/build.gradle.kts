02aafdb96a2869d640ef27ba0da6f185e791c1ed
CloudExt <me@mydomain.com>
2024-02-29 22:49:17 +0530

    added Update

--- TATATVProvider/build.gradle.kts
+++ TATATVProvider/build.gradle.kts
@@ -5,7 +5,7 @@ version = 1
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Indian Live TV TATA"
+    description = "Indian TV"
     authors = listOf("HindiProvider")
 
     /**
