efaf943a819a5f8ee198fe4e61137802ddcba759
CloudExt <me@mydomain.com>
2024-03-12 22:48:22 +0530

    Add

---
+++ actionviewphotography/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 4
+
+cloudstream {
+    authors     = listOf("Coxju")
+    language    = "hi"
+    description = "We Provide Huge Colocation of Hindi Porn (Uncut) Web Series"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=uncutmaza.cc&sz=%size%"
+}
