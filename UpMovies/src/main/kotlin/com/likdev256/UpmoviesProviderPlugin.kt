e59afc1477e6bbca9ae021f634277ad9e1cdbaaa
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-03-12 11:29:38 +0530

    Update UpmoviesProviderPlugin.kt

--- UpMovies/src/main/kotlin/com/likdev256/UpmoviesProviderPlugin.kt
+++ UpMovies/src/main/kotlin/com/likdev256/UpmoviesProviderPlugin.kt
@@ -7,6 +7,5 @@ class UpmoviesProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(UpmoviesProvider())
-        registerExtractorAPI(DoodWatchExtractor())
     }
 }
