3e1de26189bb9906749b93e371a42c5118a29d44
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-03-12 11:55:23 +0530

    Update UpmoviesProviderPlugin.kt

--- UpMovies/src/main/kotlin/com/likdev256/UpmoviesProviderPlugin.kt
+++ UpMovies/src/main/kotlin/com/likdev256/UpmoviesProviderPlugin.kt
@@ -1,3 +1,5 @@
+package com.likdev256
+
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
@@ -7,5 +9,6 @@ class UpmoviesProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(UpmoviesProvider())
+        registerExtractorAPI(DoodWatchExtractor())
     }
 }
