763609ae42466c911f347a5f054e4d785e44bae5
CloudExt <me@mydomain.com>
2024-03-27 22:29:32 +0530

    updated Extractor

--- UpMovies/src/main/kotlin/com/likdev256/UpmoviesProviderPlugin.kt
+++ UpMovies/src/main/kotlin/com/likdev256/UpmoviesProviderPlugin.kt
@@ -3,6 +3,9 @@ package com.likdev256
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.lagradost.cloudstream3.extractors.DoodCxExtractor
+import com.lagradost.cloudstream3.extractors.DoodSoExtractor
+import com.lagradost.cloudstream3.extractors.DoodWatchExtractor
 import com.lagradost.cloudstream3.extractors.StreamTape
 import com.lagradost.cloudstream3.extractors.Streamplay
 import com.lagradost.cloudstream3.extractors.Vidmoly
@@ -16,8 +19,11 @@ class UpmoviesProviderPlugin: Plugin() {
         registerExtractorAPI(EPlayExtractor())
         registerExtractorAPI(Streamwish())
         registerExtractorAPI(Filelion())
+        registerExtractorAPI(DoodCxExtractor())
         registerExtractorAPI(DoodmainExtractor())
         registerExtractorAPI(DoodReExtractor())
+        registerExtractorAPI(DoodSoExtractor())
+        registerExtractorAPI(DoodWatchExtractor())
         registerExtractorAPI(vtbe())
         registerExtractorAPI(StreamTape())
         registerExtractorAPI(Streamplay())
