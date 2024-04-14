ed05ddae192d5e712edeefd7d882a715f734383b
CloudExt <me@mydomain.com>
2024-04-14 17:50:11 +0530

    Added new Extractor in Toonstream

--- Toonstream/src/main/kotlin/com/Toonstream/ToonstreamProvider.kt
+++ Toonstream/src/main/kotlin/com/Toonstream/ToonstreamProvider.kt
@@ -3,12 +3,14 @@ package com.Toonstream
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.lagradost.cloudstream3.extractors.Vidmolyme
 
 @CloudstreamPlugin
 class ToonstreamProvider: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Toonstream())
         registerExtractorAPI(StreamSB8())
+        registerExtractorAPI(Vidmolyme())
         registerExtractorAPI(Vidstreaming())
     }
 }
