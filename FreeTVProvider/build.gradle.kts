89d307b5777cd6ebf37b44f6d8c119f7c08d189c
CloudExt <me@mydomain.com>
2024-04-15 10:07:08 +0530

    Add FreeTV with Channels

--- FreeTVProvider/build.gradle.kts
+++ FreeTVProvider/build.gradle.kts
@@ -1,10 +1,10 @@
 // use an integer for version numbers
-version = 1
+version = 2
 
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
-
+    language = "en"
     // description = "Lorem Ipsum"
     authors = listOf("HindiProviders")
 
