4330902b99956d0a812ab13886824911d99c3199
Nexus <79303560+Nexus-Gits@users.noreply.github.com>
2024-04-17 12:08:49 +0530

    Add files via upload

---
+++ DramacoolProvider/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 4
+
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+//    description = "Lorem Ipsum"
+    authors = listOf("Horis")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AsianDrama",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=dramacool.city&sz=%size%"
+}
---
+++ DramacoolProvider/src/main/kotlin/com/horis/cloudstreamplugins/DramacoolProvider.kt
@@ -0,0 +1,114 @@
+package com.horis.cloudstreamplugins
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
+import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.loadExtractor
+import org.jsoup.nodes.Element
+
+class DramacoolProvider : MainAPI() {
+    override val supportedTypes = setOf(
+        TvType.AsianDrama
+    )
+    override var lang = "en"
+
+    override var mainUrl = "https://dramacool.city"
+    override var name = "Dramacool"
+
+    override val hasMainPage = true
+
+    override val mainPage = mainPageOf(
+        "${mainUrl}/dramas/" to "Drama",
+        "${mainUrl}/movies/" to "Movies",
+        "${mainUrl}/k-shows/" to "KShow",
+        "${mainUrl}/most-watched/" to "Popular Dramas",
+    )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse? {
+        val url = if (page == 1) {
+            request.data
+        } else {
+            "${request.data}/page/$page/"
+        }
+
+        val document = app.get(url, referer = "$mainUrl/").document
+        val items = document.select(".box > li").mapNotNull {
+            it.toSearchResult()
+        }
+
+        return newHomePageResponse(request.name, items)
+    }
+
+    private fun Element.toSearchResult(): SearchResponse? {
+        val title = selectFirst("h3")?.text()?.trim() ?: return null
+        val href = fixUrlNull(selectFirst("a")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(selectFirst("img")?.attr("data-src"))
+
+        return newAnimeSearchResponse(title, LoadUrl(href, posterUrl).toJson()) {
+            this.posterUrl = posterUrl
+        }
+    }
+
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val url = "$mainUrl/?s=$query"
+        val document = app.get(url, referer = "$mainUrl/").document
+
+        val items = document.select(".list-thumb li").mapNotNull {
+            val a = it.selectFirst("a") ?: return@mapNotNull null
+            val name = it.selectFirst("h2")?.text() ?: return@mapNotNull null
+            val posterUrl = fixUrlNull(it.selectFirst("img")?.attr("src"))
+            val href = a.attr("href")
+            newMovieSearchResponse(name, LoadUrl(href, posterUrl).toJson()) {
+                this.posterUrl = posterUrl
+            }
+        }
+        return items
+    }
+
+    override suspend fun load(url: String): LoadResponse? {
+        val d = tryParseJson<LoadUrl>(url) ?: return null
+        val document = app.get(d.url, referer = "$mainUrl/").document
+        val title = document.selectFirst("h1")?.text()?.trim() ?: return null
+
+        val episodes = document.select("#all-episodes ul li").mapNotNull { el ->
+            el.select("a").mapNotNull {
+                val href = fixUrl(it.attr("data-source"))
+                newEpisode(href) {
+                    name = it.text().trim()
+                }
+            }
+        }.flatten()
+
+
+        return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
+            posterUrl = d.posterUrl
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val id = data.substringAfter("id=")
+        val json = app.get("https://dramasb.com/stream?id=$id", verify = false).text
+        val linksRegex = "\"link\":\"(.*?)\"".toRegex()
+        val servers = linksRegex.findAll(json).map {
+            it.groupValues[1].replace("\\/", "/")
+        }.toList()
+
+        servers.amap {
+            loadExtractor(it, subtitleCallback, callback)
+        }
+        return true
+    }
+
+    data class LoadUrl(
+        val url: String,
+        val posterUrl: String?
+    )
+
+}
