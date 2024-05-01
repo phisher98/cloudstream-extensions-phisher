08a1725b73a25450a82500a977a41d9356fe8705
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-05-01 16:03:14 +0530

    Update Extractors.kt

--- CodeStream/src/main/kotlin/com/hexated/Extractors.kt
+++ CodeStream/src/main/kotlin/com/hexated/Extractors.kt
@@ -670,3 +670,50 @@ open class Chillx : ExtractorApi() {
 class Graceaddresscommunity : Voe() {
     override var mainUrl = "https://graceaddresscommunity.com"
 }
+
+class Tellygossips : ExtractorApi() {
+    override val mainUrl = "https://flow.tellygossips.net"
+    override val name = "Tellygossips"
+    override val requiresReferer = false
+    private val referer = "http://tellygossips.net/"
+    private val configRegex = "var config = ([\\s\\S]*?);".toRegex()
+    private val headers = mapOf("Referer" to "$mainUrl/")
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val doc = app.get(url, referer = this.referer).document
+        val configStr = doc.select("script")
+            .map { it.data() }
+            .firstOrNull { it.contains("var config = ") }
+            ?.let { configRegex.find(it.trim())?.groupValues?.get(1) } ?: return
+        val config = tryParseJson<Config>(configStr) ?: return
+        for (link in config.sources) {
+            callback(
+                ExtractorLink(
+                    source,
+                    "$name ${link.label}",
+                    link.file ?: link.src ?: continue,
+                    "",
+                    Qualities.Unknown.value,
+                    true,
+                    headers
+                )
+            )
+        }
+    }
+    data class Config(
+        val sources: List<VideoLink>,
+    )
+
+    data class VideoLink(
+        val file: String?,
+        val src: String?,
+        val label: String,
+        val type: String,
+    )
+
+}
