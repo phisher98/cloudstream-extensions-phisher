e74c1ca8f4b381c73e67da2e9f0e46e78f9b35a4
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-05-01 16:03:09 +0530

    Update CodeExtractor.kt

--- CodeStream/src/main/kotlin/com/hexated/CodeExtractor.kt
+++ CodeStream/src/main/kotlin/com/hexated/CodeExtractor.kt
@@ -3091,6 +3091,10 @@ object CodeExtractor : CodeStream() {
                         "$PlaydesiAPI/$fixTitle-season-$season-episode-$episode-watch-online"
                     }
                     val document = app.get(url).document
-                }
+                document.select("div.entry-content > p a").attr("href").forEach
+                {
+                    loadExtractor(src, subtitleCallback, callback)
+                }  
+            }
 }
 
