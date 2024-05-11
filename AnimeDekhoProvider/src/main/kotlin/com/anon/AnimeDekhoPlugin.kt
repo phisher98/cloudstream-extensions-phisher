df56bdc058771f7a2ad2f3057e3390019844135e
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-05-11 09:18:33 +0530

    Update AnimeDekhoPlugin.kt

--- AnimeDekhoProvider/src/main/kotlin/com/anon/AnimeDekhoPlugin.kt
+++ AnimeDekhoProvider/src/main/kotlin/com/anon/AnimeDekhoPlugin.kt
@@ -7,8 +7,8 @@ import android.content.Context
 @CloudstreamPlugin
 class AnimeDekhoPlugin: Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(AnimeDekhoProvider())
-        //registerMainAPI(AnimeDekho2Provider())
+        //registerMainAPI(AnimeDekhoProvider())
+        registerMainAPI(AnimeDekho2Provider())
         registerExtractorAPI(Streamruby())
         registerExtractorAPI(VidStream())
         registerExtractorAPI(Vidmolynet())
