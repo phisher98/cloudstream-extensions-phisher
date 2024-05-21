7eef6c7c56a7a9818011ec81c7e056c397e0a812
phisher98 <mandiktest2@gmail.com>
2024-05-21 16:49:10 +0530

    Added Icon

--- Anplay/build.gradle.kts
+++ Anplay/build.gradle.kts
@@ -22,5 +22,5 @@ cloudstream {
     // You can find a list of avaliable types here:
     // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
     tvTypes = listOf("Movie,Anime,Cartoon")
-    iconUrl="https://toonstream.net/wp-content/uploads/2024/01/TOONSTREAM.png"
+    iconUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFFWqooQh6c2ojQl0JG9DOzExN3rZMwncShzuvzAUA9A&s"
 }
