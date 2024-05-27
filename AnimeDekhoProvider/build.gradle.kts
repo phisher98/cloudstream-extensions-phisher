1603b031bbe70b7e3995dcda538f499d809560ea
phisher98 <mandiktest2@gmail.com>
2024-05-27 13:07:46 +0530

    Maintenance

--- AnimeDekhoProvider/build.gradle.kts
+++ AnimeDekhoProvider/build.gradle.kts
@@ -1,4 +1,4 @@
-version = 13
+version = 14
 
 cloudstream {
     language = "hi"
@@ -18,5 +18,5 @@ cloudstream {
         "Cartoon"
     )
 
-    iconUrl = "https://animedekho.com/wp-content/uploads/2023/07/AnimeDekho-Logo-300x-1.png"
+    iconUrl = "https://animedekho.net/wp-content/uploads/2023/07/AnimeDekho-Logo-300x-1.png"
 }
