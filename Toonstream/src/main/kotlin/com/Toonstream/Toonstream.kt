f55817e0a077a0c02180782ecd6fba93bcfe9e5f
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-07-05 19:03:08 +0530

    Update Toonstream.kt

--- Toonstream/src/main/kotlin/com/Toonstream/Toonstream.kt
+++ Toonstream/src/main/kotlin/com/Toonstream/Toonstream.kt
@@ -37,7 +37,7 @@ class Toonstream : MainAPI() {
     private fun Element.toSearchResult(): SearchResponse {
         val title     = this.select("article  > header > h2").text().trim().replace("Watch Online","")
         val href      = fixUrl(this.select("article  > a").attr("href"))
-        val posterUrl = fixUrlNull(this.select("article  > div.post-thumbnail > figure > img").attr("data-src").toString())
+        val posterUrl = fixUrlNull(this.select("article  > div.post-thumbnail > figure > img").attr("src").toString())
         return newMovieSearchResponse(title, href, TvType.Movie) {
             this.posterUrl = posterUrl
         }
