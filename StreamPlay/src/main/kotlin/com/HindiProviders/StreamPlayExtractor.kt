8416afad327c2d4cd9dfa7cf0471a4ed77987f3c
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-07-21 19:55:36 +0530

    Update StreamPlayExtractor.kt

--- StreamPlay/src/main/kotlin/com/HindiProviders/StreamPlayExtractor.kt
+++ StreamPlay/src/main/kotlin/com/HindiProviders/StreamPlayExtractor.kt
@@ -1030,7 +1030,8 @@ object StreamPlayExtractor : StreamPlay() {
             "$vidsrctoAPI/embed/tv/$imdbId/$season/$episode"
         }
         Log.d("Phisher ID",url)
-        AnyVidplay(url).getUrl(url, url, subtitleCallback, callback)
+        val host = getBaseUrl(url)
+        AnyVidplay(host).getUrl(url, "", subtitleCallback, callback)
     }
 
     suspend fun invokeKisskh(
