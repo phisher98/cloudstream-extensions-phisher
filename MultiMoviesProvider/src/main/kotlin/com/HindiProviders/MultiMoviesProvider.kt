3007cf1d81058a1d5b766a97110525c544d7ea26
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-08-13 19:13:57 +0530

    Update MultiMovies Domain

--- MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProvider.kt
+++ MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProvider.kt
@@ -11,7 +11,7 @@ import com.lagradost.nicehttp.NiceResponse
 import okhttp3.FormBody
 
 class MultiMoviesProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://multimovies.online"
+    override var mainUrl = "https://multimovies.sbs"
     override var name = "MultiMovies"
     override val hasMainPage = true
     override var lang = "hi"
