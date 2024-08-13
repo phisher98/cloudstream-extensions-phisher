871ec3daed557cb02e7cfdd8c5e184d8e5bd2cfe
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-08-13 13:01:56 +0530

    Fixed Toonstream Homepage Posters

--- Toonstream/src/main/kotlin/com/Toonstream/Toonstream.kt
+++ Toonstream/src/main/kotlin/com/Toonstream/Toonstream.kt
@@ -37,9 +37,17 @@ class Toonstream : MainAPI() {
     private fun Element.toSearchResult(): SearchResponse {
         val title     = this.select("article  > header > h2").text().trim().replace("Watch Online","")
         val href      = fixUrl(this.select("article  > a").attr("href"))
-        val posterUrl = fixUrlNull(this.select("article  > div.post-thumbnail > figure > img").attr("src").toString())
-        return newMovieSearchResponse(title, href, TvType.Movie) {
-            this.posterUrl = posterUrl
+        val posterUrlRaw = fixUrlNull(this.select("article  > div.post-thumbnail > figure > img").attr("data-src").toString())
+        return if (posterUrlRaw.contains("http")) {
+            val posterUrl=posterUrlRaw
+            newMovieSearchResponse(title, href, TvType.Movie) {
+                this.posterUrl = posterUrl
+            }
+        } else {
+            val posterUrl="https:$posterUrlRaw"
+            newMovieSearchResponse(title, href, TvType.Movie) {
+                this.posterUrl = posterUrl
+            }
         }
     }
 
