d44e374bdcc43e68d1f30d6d469c65b03951c2a0
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2024-08-16 21:49:27 +0530

    Update Kissasian.kt

--- Kissasian/src/main/kotlin/com/kissasian/Kissasian.kt
+++ Kissasian/src/main/kotlin/com/kissasian/Kissasian.kt
@@ -1,162 +1,146 @@
 package com.kissasian
 
-import android.util.Log
-import com.fasterxml.jackson.annotation.JsonProperty
-import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.network.WebViewResolver
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.utils.AppUtils.parseJson
-import org.jsoup.Jsoup
-import java.net.URI
+import com.lagradost.cloudstream3.extractors.Filesim
+import com.lagradost.cloudstream3.extractors.helper.GogoHelper
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.httpsify
+import com.lagradost.cloudstream3.utils.loadExtractor
+import org.jsoup.nodes.Element
 
-class Kissasian : MainAPI() {
-    override var mainUrl              = "https://ww2.kissasian.vip"
-    override var name                 = "Kissasian"
-    override val hasMainPage          = true
-    override var lang                 = "en"
-    override val hasDownloadSupport   = true
-    override val supportedTypes       = setOf(TvType.AsianDrama,TvType.Movie)
+open class Kissasian : MainAPI() {
+    override var mainUrl = "https://kissasian.pe"
+    override var name = "Kissasian"
+    override val hasMainPage = true
+    override val hasDownloadSupport = true
+    override val supportedTypes = setOf(TvType.AsianDrama)
 
     companion object {
-        val xmlHeader = mapOf("X-Requested-With" to "XMLHttpRequest")
+        fun getStatus(t: String?): ShowStatus {
+            return when (t) {
+                "Completed" -> ShowStatus.Completed
+                "Ongoing" -> ShowStatus.Ongoing
+                else -> ShowStatus.Completed
+            }
+        }
     }
 
     override val mainPage = mainPageOf(
-        "drama-list/ongoing.html" to "Drama Ongoing",
-        "recently-added" to "Recently Added",
-        "filter?type=1&status=2&country=1&sort=updated?page=" to "Korean Drama",
-        "filter?type=2&status=2&country=1&sort=updated?page=" to "Korean Movies",
-        "filter?type=1&status=2&country=2&sort=updated?page=" to "Japanese Drama",
-        "filter?type=2&status=2&country=2&sort=updated?page=" to "Japanese Movies",
-        "filter?type=1&status=2&country=5&sort=updated?page=" to "Chinese Drama",
-        "filter?type=2&status=2&country=5&sort=updated?page=" to "Chinese Movies",
+        "drama-list/ongoing.html?page=" to "Drama Ongoing",
+        "drama-list/completed.html?page=" to "Drama Completed",
+        "genre/variety/?page=" to "Variety Show",
+        "genre/romance/?page=" to "Romance",
+        "genre/action/?page=" to "Action",
+        "genre/mystery/?page=" to "Mystery",
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("$mainUrl/${request.data}?page=$page").document
-        val home     = document.select("div.film_list-wrap > div.flw-item").mapNotNull { it.toSearchResult() }
-
-        return newHomePageResponse(
-            list    = HomePageList(
-                name               = request.name,
-                list               = home,
-                isHorizontalImages = false
-            ),
-            hasNext = true
-        )
+        val document = app.get("$mainUrl/${request.data}$page").document
+        val home = document.select("div.list-drama div.item").mapNotNull {
+            it.toSearchResult()
+        }
+        return newHomePageResponse(request.name, home)
     }
 
-    private fun Element.toSearchResult(): SearchResponse {
-        val title     = this.select(".film-detail > .film-name > a").attr("title").trim()
-        val href      = fixUrl(this.select("div.film-poster > a").attr("href")).replace("detail","watch")
-        val posterUrl = fixUrlNull(this.select("div.film-poster > img").attr("data-src"))
-        return newMovieSearchResponse(title, href, TvType.Movie) {
+    private fun Element.toSearchResult(): SearchResponse? {
+        val href = fixUrl(this.selectFirst("a")?.attr("href") ?: return null)
+        val title = this.selectFirst("span.title")?.text()?.trim() ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
+        return newTvSeriesSearchResponse(title, href, TvType.AsianDrama) {
             this.posterUrl = posterUrl
         }
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val searchResponse = mutableListOf<SearchResponse>()
-
-        for (i in 1..5) {
-            val document = app.get("${mainUrl}/search?keyword=$query&page=$i").document
-
-            val results = document.select("div.film_list-wrap > div.flw-item").mapNotNull { it.toSearchResult() }
-
-            if (!searchResponse.containsAll(results)) {
-                searchResponse.addAll(results)
-            } else {
-                break
-            }
-
-            if (results.isEmpty()) break
+        val document = app.get("$mainUrl/search.html?keyword=$query").document
+        return document.select("div.list-drama div.item").mapNotNull {
+            it.toSearchResult()
         }
-
-        return searchResponse
     }
 
-    override suspend fun load(url: String): LoadResponse {
+    open val contentInfoClass = "barContentInfo"
+    override suspend fun load(url: String): LoadResponse? {
         val document = app.get(url).document
-        val title = document.selectFirst(".anisc-detail > .film-name")?.text().toString()
-        val poster = document.selectFirst(".anisc-poster img")?.attr("src")
-        val description = document.selectFirst(".film-description.m-hide > .text")?.text()
-        val tvtag=document.selectFirst("div.film-stats > span.item")?.text().toString()
-        val tvType =
-            if (tvtag=="MOVIE") TvType.Movie
-           else TvType.AsianDrama
-        val showname = URI(url).path.split("/").last()
-        return if (tvType == TvType.AsianDrama) {
-            val episodes =
-                Jsoup.parse(
-                    parseJson<Response>(
-                        app.get(
-                            "$mainUrl/ajax/v2/episode/list/$showname"
-                        ).text
-                    ).html
-                ).select(".ss-list > a[href].ssl-item.ep-item").map {
-                    newEpisode(showname+it.attr("href")) {
-                        this.name = "Episode"+it.attr("player-title")
-                        this.episode = it.selectFirst(".ssli-order")?.text()?.toIntOrNull()
-                    }
-                }
-            //Log.d("Phisher Epe", "$episodes")
-            newTvSeriesLoadResponse(title, url, TvType.AsianDrama, episodes) {
-                this.posterUrl = poster
-                this.plot = description
+
+        val title = document.selectFirst("div.$contentInfoClass a")?.text()?.trim() ?: return null
+        val poster = fixUrlNull(document.select("div.$contentInfoClass img").last()?.attr("src"))
+        val tags = document.select("div.$contentInfoClass p:contains(Genres:) a").map { it.text().removePrefix(",").trim() }
+
+        val year = document.selectFirst("div.$contentInfoClass p.type.Releasea")?.text()?.trim()?.toIntOrNull()
+        val status = getStatus(document.selectFirst("div.$contentInfoClass p:contains(Status:)")?.ownText()?.trim())
+        val description = document.selectFirst("div.$contentInfoClass p.des, div.$contentInfoClass p:last-child")?.nextElementSiblings()?.select("p")?.text()
+
+        val episodes = document.select("ul.listing li, table.listing td.episodeSub").map {
+            val name = it.selectFirst("a")?.attr("title")
+            val link = fixUrlNull(it.selectFirst("a")?.attr("href"))
+            val epNum = Regex("Episode\\s(\\d+)").find("$name")?.groupValues?.getOrNull(1)?.toIntOrNull()
+            newEpisode(link) {
+                this.name = name
+                this.episode = epNum
+            }
+        }.reversed()
+
+        if (episodes.size == 1) {
+            return newMovieLoadResponse(title, url, TvType.Movie, episodes[0].data) {
+                posterUrl = poster
+                this.year = year
+                plot = description
+                this.tags = tags
             }
         } else {
-            newMovieLoadResponse(title, url, TvType.Movie, url) {
-                this.posterUrl = poster
-                this.plot = description
+            return newTvSeriesLoadResponse(title, url, TvType.AsianDrama, episodes = episodes) {
+                posterUrl = poster
+                this.year = year
+                showStatus = status
+                plot = description
+                this.tags = tags
             }
         }
     }
 
-    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
-        Log.d("Phisher Data",data)
-        val parts = data.split("?ep=")
-        val showname = parts[0].substringAfterLast("/")
-        val ep = parts[1]
-        Jsoup.parse(
-            parseJson<Response>(
-                app.get(
-                    "$mainUrl/ajax/v2/episode/servers?episodeId=$showname-$ep"
-                ).text
-            ).html
-        ).select("div.ps__-list > div.item.server-item").forEach {
-            val servername=it.text()
-            val serverid=it.attr("data-id")
-            val apiRes =
-                app.get("$mainUrl/ajax/v2/episode/sources?id=$serverid")
-                    .parsedSafe<APISourceresponse>()
-            val fstream=apiRes?.link.toString()
-            val response = app.get(fstream,referer = mainUrl, headers = xmlHeader, interceptor = WebViewResolver(Regex("""ajax/getSources""")))
-            val extractdata=response.text.replace("\": ","\":")
-            val regex = """"file":"(.*?)""""
-            val matchResult = regex.toRegex().find(extractdata)
-            val source = matchResult?.groups?.get(1)?.value ?:""
-                    callback.invoke(
-                        ExtractorLink(
-                            source = name,
-                            name = "$name $servername",
-                            url = source,
-                            referer = "$mainUrl/",
-                            quality = Qualities.P1080.value,
-                            type = INFER_TYPE
-                        )
-                    )
-        }
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+
+        val document = app.get(data).document
+        val server = document.selectFirst("select#selectServer option")?.attr("value")
+        val iframe = app.get(httpsify(server ?: return false))
+        val iframeDoc = iframe.document
+
+        argamap({
+            iframeDoc.select(".list-server-items > .linkserver")
+                .forEach { element ->
+                    val status = element.attr("data-status") ?: return@forEach
+                    if (status != "1") return@forEach
+                    val extractorData = element.attr("data-video") ?: return@forEach
+                    loadExtractor(extractorData, iframe.url, subtitleCallback, callback)
+                }
+        }, {
+            val iv = "9262859232435825"
+            val secretKey = "93422192433952489752342908585752"
+            val secretDecryptKey = secretKey
+            GogoHelper.extractVidstream(
+                iframe.url,
+                this.name,
+                callback,
+                iv,
+                secretKey,
+                secretDecryptKey,
+                isUsingAdaptiveKeys = false,
+                isUsingAdaptiveData = true,
+                iframeDocument = iframeDoc
+            )
+        })
+
         return true
     }
 
-    private data class Response(
-        @JsonProperty("status") val status: Boolean,
-        @JsonProperty("html") val html: String
-    )
+}
 
-    data class APISourceresponse(
-        @JsonProperty("type") val type: String,
-        @JsonProperty("link") val link: String? = null,
-    )
+class Kswplayer : Filesim() {
+    override val name = "Kswplayer"
+    override var mainUrl = "https://kswplayer.info"
 }
