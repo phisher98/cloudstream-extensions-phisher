5dc377c9ae44378b6cdb0c07ec1e3646af0bdf14
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2024-08-17 09:46:26 +0530

    Fix dramacool

--- Kissasian/src/main/kotlin/com/kissasian/KissasianProvider.kt
+++ Kissasian/src/main/kotlin/com/kissasian/KissasianProvider.kt
@@ -5,6 +5,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 import com.lagradost.cloudstream3.extractors.MixDrop
 import com.lagradost.cloudstream3.extractors.StreamTape
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.extractorApis
 
 @CloudstreamPlugin
 class KissasianProvider: Plugin() {
@@ -17,5 +19,10 @@ class KissasianProvider: Plugin() {
         registerExtractorAPI(StreamTape())
         registerExtractorAPI(MixDrop())
         registerExtractorAPI(Kswplayer())
+        addExtractor(Vidstream2("https://asianembed.io"))
+    }
+    private fun addExtractor(element: ExtractorApi) {
+        element.sourcePlugin = __filename
+        extractorApis.add(0, element)
     }
 }
