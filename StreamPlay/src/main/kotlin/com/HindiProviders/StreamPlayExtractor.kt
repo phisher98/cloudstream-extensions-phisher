56413fada6cbafa66e1cb9b59662a98539d5f429
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-08-20 20:50:18 +0530

    Update StreamPlayExtractor.kt

--- StreamPlay/src/main/kotlin/com/HindiProviders/StreamPlayExtractor.kt
+++ StreamPlay/src/main/kotlin/com/HindiProviders/StreamPlayExtractor.kt
@@ -22,6 +22,8 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import org.mozilla.javascript.Scriptable
+import com.lagradost.cloudstream3.extractors.VidSrcTo
+
 
 val session = Session(Requests().baseClient)
 
