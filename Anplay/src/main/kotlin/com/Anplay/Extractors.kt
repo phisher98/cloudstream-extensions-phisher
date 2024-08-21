911d7d4d29daf3a2dbf7ca4e7f65b55425f7f079
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-08-21 12:38:01 +0530

    Update Extractors.kt

--- Anplay/src/main/kotlin/com/Anplay/Extractors.kt
+++ Anplay/src/main/kotlin/com/Anplay/Extractors.kt
@@ -28,6 +28,8 @@ class Watchx : Chillx() {
     override val mainUrl = "https://watchx.top"
 }
 
+/*
+
 open class Chillx : ExtractorApi() {
     override val name = "Chillx"
     override val mainUrl = "https://chillx.top"
@@ -82,7 +84,7 @@ open class Chillx : ExtractorApi() {
         }
         // required
         val headers = mapOf(
-            "Accept" to "*/*",
+         "Accept" to "*//*",
             "Connection" to "keep-alive",
             "Sec-Fetch-Dest" to "empty",
             "Sec-Fetch-Mode" to "cors",
@@ -96,7 +98,98 @@ open class Chillx : ExtractorApi() {
             "$mainUrl/",
             headers = headers
         ).forEach(callback)
+    }*/
+
+    open class Chillx : ExtractorApi() {
+    override val name = "Chillx"
+    override val mainUrl = "https://chillx.top"
+    override val requiresReferer = true
+	private var key: String? = null
+
+    @Suppress("NAME_SHADOWING")
+	override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {		
+		val master = Regex("\\s*=\\s*'([^']+)").find(
+            app.get(
+                url,
+                referer = referer ?: "",
+                headers = mapOf(
+                    "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                    "Accept-Language" to "en-US,en;q=0.5",
+                )
+            ).text
+        )?.groupValues?.get(1)
+
+		val key = fetchKey() ?: throw ErrorLoadingException("Unable to get key")
+
+        val decrypt = cryptoAESHandler(master ?: return, key.toByteArray(), false)
+            ?.replace("\\", "")
+            ?: throw ErrorLoadingException("failed to decrypt")
+
+        val source = Regex(""""?file"?:\s*"([^"]+)""").find(decrypt)?.groupValues?.get(1)
+        val name = url.getHost()
+
+        val subtitles = Regex("""subtitle"?:\s*"([^"]+)""").find(decrypt)?.groupValues?.get(1)
+        val subtitlePattern = """\[(.*?)\](https?://[^\s,]+)""".toRegex()
+        val matches = subtitlePattern.findAll(subtitles ?: "")
+        val languageUrlPairs = matches.map { matchResult ->
+            val (language, url) = matchResult.destructured
+	@@ -79,28 +122,51 @@ open class Chillx : ExtractorApi() {
+                )
+            )
+        }
+
+        val header =
+            mapOf(
+                "accept" to "*/*",
+                "accept-language" to "en-US,en;q=0.5",
+                "Origin" to mainUrl,
+                "Accept-Encoding" to "gzip, deflate, br",
+                "Connection" to "keep-alive",
+                "Sec-Fetch-Dest" to "empty",
+                "Sec-Fetch-Mode" to "cors",
+                "Sec-Fetch-Site" to "cross-site",
+                "user-agent" to USER_AGENT,
+            )
+
+		callback.invoke(
+            ExtractorLink(
+                name,
+                name,
+                url = source ?: return,
+                referer = "$mainUrl/",
+                quality = Qualities.Unknown.value,
+				INFER_TYPE,
+                headers = header,
+            )
+        )
+    }	
+
+    private suspend fun fetchKey(): String? {
+        return app.get("https://raw.githubusercontent.com/Rowdy-Avocado/multi-keys/keys/index.html")
+            .parsedSafe<Keys>()?.key?.get(0)?.also { key = it }
     }
+	
+	private fun decodeUnicodeEscape(input: String): String {
+        val regex = Regex("u([0-9a-fA-F]{4})")
+        return regex.replace(input) {
+            it.groupValues[1].toInt(16).toChar().toString()
+        }
+    }
+
+	private fun String.getHost(): String {
+		return fixTitle(URI(this).host.substringBeforeLast(".").substringAfterLast("."))
+	}
+
+	data class Keys(
+        @JsonProperty("chillx") val key: List<String>
+    )
+
+}
 
     private fun decodeUnicodeEscape(input: String): String {
         val regex = Regex("u([0-9a-fA-F]{4})")
@@ -105,10 +198,5 @@ open class Chillx : ExtractorApi() {
         }
     }
 
-
-
-    data class Keys(
-        @JsonProperty("chillx") val key: List<String>
-    )
 
 }
