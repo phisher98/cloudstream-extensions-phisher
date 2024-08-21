47e0c428d45f60c4b209d27494ce3cda3b1e7b24
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-08-21 12:57:30 +0530

    Update StreamPlayExtractor.kt

--- StreamPlay/src/main/kotlin/com/HindiProviders/StreamPlayExtractor.kt
+++ StreamPlay/src/main/kotlin/com/HindiProviders/StreamPlayExtractor.kt
@@ -1011,7 +1011,7 @@ object StreamPlayExtractor : StreamPlay() {
             "$vidsrctoAPI/embed/tv/$imdbId/$season/$episode"
         }
         Log.d("Phisher ID",url)
-        AnyVidSrcTo(url).getUrl(iFrameUrl, url, subtitleCallback, callback)
+        AnyVidSrcTo(url).getUrl(url, url, subtitleCallback, callback)
 
         class AnyVidSrcTo(domain: String) : VidSrcTo() {
 		override val mainUrl = domain
