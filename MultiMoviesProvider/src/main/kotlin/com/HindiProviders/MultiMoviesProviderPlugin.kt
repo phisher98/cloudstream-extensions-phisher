5b07f4ee88df0d0ac2471a58017262f09ac70d21
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-08-29 12:56:48 +0530

    Update

--- MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProviderPlugin.kt
+++ MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProviderPlugin.kt
@@ -6,14 +6,13 @@ import android.content.Context
 import com.lagradost.cloudstream3.extractors.DoodLaExtractor
 import com.lagradost.cloudstream3.extractors.MixDrop
 import com.lagradost.cloudstream3.extractors.XStreamCdn
-import com.lagradost.cloudstream3.extractors.VidHidePro5
 
 @CloudstreamPlugin
 class MultiMoviesProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(MultiMoviesProvider())
-        registerExtractorAPI(VidHidePro5())
+        registerExtractorAPI(VidHideProMulti())
         registerExtractorAPI(MixDrop())
         registerExtractorAPI(Multimovies())
         registerExtractorAPI(XStreamCdn())
@@ -21,8 +20,9 @@ class MultiMoviesProviderPlugin: Plugin() {
         registerExtractorAPI(Animezia())
         registerExtractorAPI(server2())
         registerExtractorAPI(MultimoviesAIO())
-        registerExtractorAPI(GDMirrorbot())
+        //registerExtractorAPI(GDMirrorbot())
         registerExtractorAPI(VidhideExtractor())
         registerExtractorAPI(Asnwish())
-            }
+        registerExtractorAPI(CdnwishCom())
+        }
 }
