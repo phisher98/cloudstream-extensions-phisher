a397155b63f9d36af953d05b4160ea67631d26c5
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-08-29 13:01:05 +0530

    update

--- MultiMoviesProvider/build.gradle.kts
+++ MultiMoviesProvider/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.konan.properties.Properties
 // use an integer for version numbers
-version = 16
+version = 13
 
 
 android {
--- MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProviderPlugin.kt
+++ MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProviderPlugin.kt
@@ -6,13 +6,14 @@ import android.content.Context
 import com.lagradost.cloudstream3.extractors.DoodLaExtractor
 import com.lagradost.cloudstream3.extractors.MixDrop
 import com.lagradost.cloudstream3.extractors.XStreamCdn
+import com.lagradost.cloudstream3.extractors.VidHidePro5
 
 @CloudstreamPlugin
 class MultiMoviesProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(MultiMoviesProvider())
-        registerExtractorAPI(VidHideProMulti())
+        registerExtractorAPI(VidHidePro5())
         registerExtractorAPI(MixDrop())
         registerExtractorAPI(Multimovies())
         registerExtractorAPI(XStreamCdn())
