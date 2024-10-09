bfd95a683985b2523ba8b9527f849373e2db6751
Phisher98 <mandiktest2@gmail.com>
2024-10-10 02:03:32 +0530

    Added FanCode Live Extension

---
+++ FanCode/build.gradle.kts
@@ -0,0 +1,38 @@
+@file:Suppress("UnstableApiUsage")
+
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 1
+
+android {
+    defaultConfig {
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        buildConfigField("String", "FanCode_API", "\"${properties.getProperty("FanCode_API")}\"")
+
+    }
+}
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "FanCode Streams"
+    language    = "en"
+    authors = listOf("Phisher98")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of avaliable types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf("Live")
+
+    iconUrl="https://www.fancode.com/skillup-uploads/fc-web-logo/fc_logo_white_bg.svg"
+}
---
+++ FanCode/src/main/kotlin/com/FanCode/FanCode.kt
@@ -0,0 +1,62 @@
+package com.FanCode
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+
+class FanCode : MainAPI() {
+    override var mainUrl: String = BuildConfig.FanCode_API
+    override var name = "FanCode"
+    override val hasMainPage = true
+    override var lang = "en"
+    override val supportedTypes = setOf(TvType.Live)
+
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val data = app.get(mainUrl).parsedSafe<Root>()
+        val home = data?.matches?.amap {
+            it.toSearchResult()
+        }
+        return newHomePageResponse("Matches", home!!)
+    }
+
+    private fun Match.toSearchResult(): SearchResponse {
+        val title = this.matchName
+        val href = this.streamLink
+        val posterUrl = this.banner
+        return newMovieSearchResponse(title, href, TvType.Live) {
+            this.posterUrl = posterUrl
+        }
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        val title = "FanCode"
+        val poster =
+            "https://www.fancode.com/skillup-uploads/fc-web/home-page-new-arc/hero-image/v1/hero-image-dweb-v4.png"
+        val description =
+            "FanCode was founded in 2019 by Yannick Colaco and Prasana Krishnan. It is an over-the-top streaming service and sports e-commerce company in India. It is part of the Dream Sports group"
+
+        return newMovieLoadResponse(title, url, TvType.Live, url) {
+            this.posterUrl = poster
+            this.plot = description
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit,
+    ): Boolean {
+        callback.invoke(
+            ExtractorLink(
+                name,
+                name,
+                data,
+                "",
+                getQualityFromName(""),
+                type = INFER_TYPE,
+            )
+        )
+        return true
+    }
+}
