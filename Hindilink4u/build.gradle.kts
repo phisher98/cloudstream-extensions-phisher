da238f48de4e66e613caf3d2d25459428b110af5
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2024-10-19 21:57:26 +0530

    Add files via upload

---
+++ Hindilink4u/build.gradle.kts
@@ -0,0 +1,27 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    description ="movies and series"
+    authors = listOf("Dilip")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of available types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+    language = "hi"
+
+    iconUrl = "https://www.hindilinks4u.pics/wp-content/uploads/2021/04/favicon.png"
+}
