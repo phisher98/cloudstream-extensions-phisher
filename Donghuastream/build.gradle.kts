654f6f9958d6a9f95aaa33cdce1e90c51cb6bf84
Phisher98 <mandiktest2@gmail.com>
2024-12-11 15:42:22 +0530

    Adding Donghuastream

---
+++ Donghuastream/build.gradle.kts
@@ -0,0 +1,26 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Anime (Chinese)"
+    language    = "cn"
+    authors = listOf("Phisher98")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of avaliable types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf("Anime")
+    iconUrl="https://raw.githubusercontent.com/Kohi-den/extensions-source/927f9c68fd64ce52f6212ae633d0f0585eca3545/src/en/donghuastream/res/mipmap-xxxhdpi/ic_launcher.png"
+}
---
+++ Donghuastream/src/main/kotlin/com/Donghuastream/Donghuastream.kt
@@ -0,0 +1,137 @@
+package com.Donghuastream
+
+//import android.util.Log
+import android.annotation.SuppressLint
+import com.lagradost.api.Log
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import org.jsoup.Jsoup
+
+class Donghuastream : MainAPI() {
+    override var mainUrl              = "https://donghuastream.org"
+    override var name                 = "Donghuastream"
+    override val hasMainPage          = true
+    override var lang                 = "cn"
+    override val hasDownloadSupport   = true
+    override val supportedTypes       = setOf(TvType.Anime)
+
+    override val mainPage = mainPageOf(
+        "anime/?status=&type=&order=update&page=" to "Recently Updated",
+        "anime/?status=completed&type=&order=update" to "Completed",
+        "anime/?status=&type=special&sub=&order=update" to "Special Anime",
+    )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val document = app.get("$mainUrl/${request.data}$page").document
+        val home     = document.select("div.listupd > article").mapNotNull { it.toSearchResult() }
+
+        return newHomePageResponse(
+            list    = HomePageList(
+                name               = request.name,
+                list               = home,
+                isHorizontalImages = false
+            ),
+            hasNext = true
+        )
+    }
+
+    private fun Element.toSearchResult(): SearchResponse {
+        val title     = this.select("div.bsx > a").attr("title")
+        val href      = fixUrl(this.select("div.bsx > a").attr("href"))
+        val posterUrl = fixUrlNull(this.select("div.bsx a img").attr("data-src"))
+        return newMovieSearchResponse(title, href, TvType.Movie) {
+            this.posterUrl = posterUrl
+        }
+    }
+
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val searchResponse = mutableListOf<SearchResponse>()
+
+        for (i in 1..3) {
+            val document = app.get("${mainUrl}/page/$i/?s=$query").document
+
+            val results = document.select("div.listupd > article").mapNotNull { it.toSearchResult() }
+
+            if (!searchResponse.containsAll(results)) {
+                searchResponse.addAll(results)
+            } else {
+                break
+            }
+
+            if (results.isEmpty()) break
+        }
+
+        return searchResponse
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url).document
+        val title       = document.selectFirst("h1.entry-title")?.text()?.trim().toString()
+        val href=document.selectFirst(".eplister li > a")?.attr("href") ?:""
+        var poster = document.select("div.ime > img").attr("data-src").toString()
+        val description = document.selectFirst("div.entry-content")?.text()?.trim()
+        val type=document.selectFirst(".spe")?.text().toString()
+        val tvtag=if (type.contains("Movie")) TvType.Movie else TvType.TvSeries
+        return if (tvtag == TvType.TvSeries) {
+            val Eppage= document.selectFirst(".eplister li > a")?.attr("href") ?:""
+            val doc= app.get(Eppage).document
+            @Suppress("NAME_SHADOWING") val episodes=doc.select("div.episodelist > ul > li").map { info->
+                        val href = info.select("a").attr("href") ?:""
+                        val episode = info.select("a span").text().substringAfter("-").substringBeforeLast("-")
+                        val poster=info.selectFirst("a img")?.attr("data-src") ?:""
+                        Episode(href, episode, posterUrl = poster)
+            }
+            if (poster.isEmpty())
+            {
+                poster=document.selectFirst("meta[property=og:image]")?.attr("content")?.trim().toString()
+            }
+            newTvSeriesLoadResponse(title, url, TvType.Anime, episodes.reversed()) {
+                this.posterUrl = poster
+                this.plot = description
+            }
+        } else {
+            if (poster.isEmpty())
+            {
+                poster=document.selectFirst("meta[property=og:image]")?.attr("content")?.trim().toString()
+            }
+            newMovieLoadResponse(title, url, TvType.Movie, href) {
+                this.posterUrl = poster
+                this.plot = description
+            }
+        }
+    }
+
+    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
+        val document = app.get(data).document
+        document.select(".mobius option").amap { server->
+            val base64 = server.attr("value")
+            Log.d("Phisher", base64.toString())
+            val doc= base64Decode(base64).let { Jsoup.parse(it) }
+            val url= httpsify(doc.select("iframe").attr("src"))
+            if (url.contains("vidmoly"))
+            {
+                val newurl=url.substringAfter("=\"").substringBefore("\"")
+                val link= "http:$newurl"
+                loadExtractor(link,referer = url,subtitleCallback, callback)
+            } else if(url.contains(".mp4")) {
+                callback.invoke(
+                    ExtractorLink(
+                        "All Sub Player",
+                        "All Sub Player",
+                        url,
+                        "",
+                        getQualityFromName(""),
+                        type = INFER_TYPE,
+                    )
+                )
+            }
+            else {
+                loadExtractor(url, referer = url, subtitleCallback, callback)
+            }
+        }
+        return true
+    }
+}
