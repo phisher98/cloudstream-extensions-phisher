eb09d5542204e2d69d4c1be1ad8e99beb86d7518
Phisher98 <mandiktest2@gmail.com>
2024-12-17 22:32:34 +0530

    Silly mistake and Minor Fixes

--- StreamPlay/build.gradle.kts
+++ StreamPlay/build.gradle.kts
@@ -3,7 +3,7 @@
 import org.jetbrains.kotlin.konan.properties.Properties
 
 // use an integer for version numbers
-version = 102
+version = 103
 android {
     defaultConfig {
         val properties = Properties()
