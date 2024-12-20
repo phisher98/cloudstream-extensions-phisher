28722a2d439d74d7b93f5eeb002e4e7d06d2f078
Phisher98 <mandiktest2@gmail.com>
2024-12-20 13:33:23 +0530

    Gojo source fix

--- StreamPlay/build.gradle.kts
+++ StreamPlay/build.gradle.kts
@@ -3,7 +3,7 @@
 import org.jetbrains.kotlin.konan.properties.Properties
 
 // use an integer for version numbers
-version = 103
+version = 104
 android {
     defaultConfig {
         val properties = Properties()
