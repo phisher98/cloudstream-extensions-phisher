05c6781a95228569ff3044faa821a48ddd83e036
Phisher98 <mandiktest2@gmail.com>
2025-01-02 23:59:06 +0530

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,8 +49,8 @@ subprojects {
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
 
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
