5e1da27c9878de3434efcbea5c83022031e00419
keyiflerolsun <keyiflerolsun@gmail.com>
2025-01-05 23:11:03 +0300

    🕊
    🕊

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.Luna712:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
