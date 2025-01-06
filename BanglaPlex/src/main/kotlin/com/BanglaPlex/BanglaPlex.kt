e37666e895e972fcf68fbf30c3586cc95e021d29
Phisher98 <mandiktest2@gmail.com>
2025-01-06 22:32:28 +0530

    Fix Banglaplex

--- BanglaPlex/src/main/kotlin/com/BanglaPlex/BanglaPlex.kt
+++ BanglaPlex/src/main/kotlin/com/BanglaPlex/BanglaPlex.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 
 class Banglaplex : MainAPI() {
-    override var mainUrl              = "https://banglaplex.shop"
+    override var mainUrl              = "https://banglaplex.lol"
     override var name                 = "Banglaplex"
     override val hasMainPage          = true
     override var lang                 = "hi"
