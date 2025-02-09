e753a60af5db8b509e6d51968be581eeec336393
Phisher98 <mandiktest2@gmail.com>
2025-02-09 20:16:55 +0530

    Adding #350

---
+++ LayarKacaProvider/build.gradle.kts
@@ -0,0 +1,30 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    language = "id"
+    // All of these properties are optional, you can safely remove them
+
+    // description = "Lorem Ipsum"
+    authors = listOf("Hexated,Phisher98")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Movie",
+    )
+
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=tv7.lk21.am.in&sz=%size%"
+
+    isCrossPlatform = true
+}
---
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/Extractors.kt
@@ -0,0 +1,72 @@
+package com.layarKacaProvider
+
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.extractors.Filesim
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.INFER_TYPE
+import com.lagradost.cloudstream3.utils.getQualityFromName
+
+
+open class Hownetwork : ExtractorApi() {
+    override val name = "Hownetwork"
+    override val mainUrl = "https://stream.hownetwork.xyz"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+            url: String,
+            referer: String?,
+            subtitleCallback: (SubtitleFile) -> Unit,
+            callback: (ExtractorLink) -> Unit
+    ) {
+        val id = url.substringAfter("id=")
+        val res = app.post(
+                "$mainUrl/api.php?id=$id",
+                data = mapOf(
+                        "r" to "https://playeriframe.shop/",
+                        "d" to "stream.hownetwork.xyz",
+                ),
+                referer = url,
+                headers = mapOf(
+                        "X-Requested-With" to "XMLHttpRequest"
+                )
+        ).parsedSafe<Sources>()
+
+        res?.data?.map {
+            callback.invoke(
+                    ExtractorLink(
+                            this.name,
+                            this.name,
+                            it.file,
+                            url,
+                            getQualityFromName(it.label),
+                            INFER_TYPE
+                    )
+            )
+        }
+
+    }
+
+    data class Sources(
+            val data: ArrayList<Data>
+    ) {
+        data class Data(
+                val file: String,
+                val label: String?,
+        )
+    }
+}
+
+class Furher : Filesim() {
+    override val name = "Furher"
+    override var mainUrl = "https://furher.in"
+}
+
+class Furher2 : Filesim() {
+    override val name = "Furher 2"
+    override var mainUrl = "723qrh1p.fun"
+}
+
+
+
