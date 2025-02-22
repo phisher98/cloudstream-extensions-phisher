ac4a13b1379d756ad71ab384193285d15b839a78
kerimmkirac <127618379+kerimmkirac@users.noreply.github.com>
2025-02-22 16:50:59 +0300

    Update Dramacool.kt

--- DramacoolProvider/src/main/kotlin/com/phisher98/Dramacool.kt
+++ DramacoolProvider/src/main/kotlin/com/phisher98/Dramacool.kt
@@ -38,7 +38,7 @@ open class Dramacool : MainAPI() {
     }
 
     private fun Element.toSearchResult(): SearchResponse? {
-        val title = selectFirst("h3")?.text() ?: return null
+        val title = selectFirst("h2")?.text() ?: return null
         val href = fixUrlNull(selectFirst("a")?.attr("href")) ?: return null
         val posterUrl = fixUrlNull(selectFirst("a img")?.attr("data-original"))
         return newTvSeriesSearchResponse(title, href, TvType.TvSeries) {
@@ -49,7 +49,7 @@ open class Dramacool : MainAPI() {
 
     override suspend fun search(query: String): List<SearchResponse> {
         val searchtitle=query.createSlug()
-        val url = "$mainUrl/search?type=movies&keyword=$searchtitle"
+        val url = "$mainUrl/?type=movies&s=$searchtitle"
         val document = app.get(url, referer = "$mainUrl/").document
         val items = document.select("ul.switch-block.list-episode-item li").mapNotNull {
             it.toSearchResult()
@@ -63,13 +63,8 @@ open class Dramacool : MainAPI() {
             url.substringBefore("-episode").replace("video-watch", "drama-detail")
         val detailsDocument = app.get(detailsUrl, referer = "$mainUrl/", timeout = 10L).document
         val title = detailsDocument.selectFirst("h1")?.text()?.trim() ?: ""
-        val actors = detailsDocument.select("div.item a").map {
-            Actor(
-                it.select("h3").text(),
-                it.select("img").attr("src")
-            )
-        }
-        val description= detailsDocument.selectFirst("div.info > p:nth-of-type(4)")?.text()?.trim() ?: ""
+        
+        val description= detailsDocument.selectFirst("div.info > p:nth-of-type(3)")?.text()?.trim() ?: ""
         val posterurl = detailsDocument.selectFirst("div.details img")?.attr("src")
         val episodes = detailsDocument.select("ul.list-episode-item-2 li").mapNotNull { el ->
             val name=el.selectFirst("a h3")?.text()?.substringAfter("Episode")?.trim()
