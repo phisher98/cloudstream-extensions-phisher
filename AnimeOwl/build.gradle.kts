ee1114f78f57fe389ee4c3841c6681c01486fc95
Phisher98 <mandiktest2@gmail.com>
2025-02-26 10:52:42 +0530

    Icon Fix

--- AnimeOwl/build.gradle.kts
+++ AnimeOwl/build.gradle.kts
@@ -22,7 +22,7 @@ cloudstream {
     // You can find a list of available types here:
     // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
     tvTypes = listOf("AnimeMovie","Anime","Cartoon")
-    iconUrl = "https://animeowl.live/assets/owl-logo-white-long.webp"
+    iconUrl = "https://animeowl.me/assets/owl-logo-white-long.webp"
 
     isCrossPlatform = true
 }
