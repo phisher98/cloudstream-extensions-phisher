a8a90086536aae152d1c9b6d6c8760f4895127f3
Phisher98 <mandiktest2@gmail.com>
2025-03-31 17:42:42 +0530

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT") {
-            exclude(group = "com.github.vidstige", module = "jadb")
-        }
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
