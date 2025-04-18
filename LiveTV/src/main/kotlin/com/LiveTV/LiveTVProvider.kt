d561fbfe50d2ae9b9e67280974f8713c364d8be6
Phisher98 <mandiktest2@gmail.com>
2025-04-18 20:37:48 +0530

    Adding Live TV New Provider

---
+++ LiveTV/src/main/kotlin/com/LiveTV/LiveTVProvider.kt
@@ -0,0 +1,12 @@
+package com.LiveTV
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class LiveTVProvider: BasePlugin() {
+    override fun load() {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(LiveTV())
+    }
+}
