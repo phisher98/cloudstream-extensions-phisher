c826436ed67c5f43686ca790b6ccef0f7b183a99
Phisher98 <mandiktest2@gmail.com>
2025-05-06 22:18:40 +0530

    Fix Kisskh Subtitle

--- KisskhProvider/src/main/kotlin/com/Phisher98/SubDecryptor.kt
+++ KisskhProvider/src/main/kotlin/com/Phisher98/SubDecryptor.kt
@@ -1,30 +1,39 @@
 package com.phisher98
 
 import com.lagradost.cloudstream3.base64DecodeArray
-import javax.crypto.BadPaddingException
 import javax.crypto.Cipher
-import javax.crypto.IllegalBlockSizeException
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 private const val KEY = "AmSmZVcH93UQUezi"
+private const val KEY2 = "8056483646328763"
+
 private val IV = intArrayOf(1382367819, 1465333859, 1902406224, 1164854838)
+private val IV2 = intArrayOf(909653298, 909193779, 925905208, 892483379)
+
 
 fun decrypt(encryptedB64: String): String {
-    return try {
-        val keyBytes = KEY.toByteArray(Charsets.UTF_8)
-        val ivBytes = IV.toByteArray()
-        val encryptedBytes = base64DecodeArray(encryptedB64) // Decode Base64 input
-        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
-        cipher.init(Cipher.DECRYPT_MODE, SecretKeySpec(keyBytes, "AES"), IvParameterSpec(ivBytes))
-        String(cipher.doFinal(encryptedBytes), Charsets.UTF_8)
-    } catch (ex: BadPaddingException) {
-        "Decryption failed: Invalid padding"
-    } catch (ex: IllegalBlockSizeException) {
-        "Decryption failed: Incorrect block size"
-    } catch (ex: Exception) {
-        "Decryption failed: ${ex.message}"
+    val keyIvPairs = listOf(
+        Pair(KEY.toByteArray(Charsets.UTF_8), IV.toByteArray()),
+        Pair(KEY2.toByteArray(Charsets.UTF_8), IV2.toByteArray())
+    )
+
+    val encryptedBytes = base64DecodeArray(encryptedB64) // Decode Base64 input
+
+    for ((keyBytes, ivBytes) in keyIvPairs) {
+        try {
+            return decryptWithKeyIv(keyBytes, ivBytes, encryptedBytes)
+        } catch (ex: Exception) {
+            println("Decryption attempt failed with key/IV pair. Error: ${ex.message}")
+        }
     }
+    return "Decryption failed: All keys/IVs failed"
+}
+
+private fun decryptWithKeyIv(keyBytes: ByteArray, ivBytes: ByteArray, encryptedBytes: ByteArray): String {
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    cipher.init(Cipher.DECRYPT_MODE, SecretKeySpec(keyBytes, "AES"), IvParameterSpec(ivBytes))
+    return String(cipher.doFinal(encryptedBytes), Charsets.UTF_8)
 }
 
 private fun IntArray.toByteArray(): ByteArray {
