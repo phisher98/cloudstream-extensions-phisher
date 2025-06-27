14f0c6b245258808e0de726c01bfcaf01272af3c
Phisher98 <mandiktest2@gmail.com>
2025-06-27 17:56:14 +0530

    Minor Optimization

--- FourKHDHub/build.gradle.kts
+++ FourKHDHub/build.gradle.kts
@@ -24,5 +24,5 @@ cloudstream {
     tvTypes = listOf("Movie","TvSeries")
     iconUrl = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/Icons/4KHDHUB-Bright-Logo.png"
 
-    isCrossPlatform = true
+    isCrossPlatform = false
 }
