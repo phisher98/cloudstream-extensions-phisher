88d5a6dd1e9d46d9667e3b90df667f48b7db526d
Phisher98 <mandiktest2@gmail.com>
2025-07-14 21:44:11 +0530

    Minor Extractor fix

--- MyFlixer/src/main/kotlin/com/phisher98/Extractors.kt
+++ MyFlixer/src/main/kotlin/com/phisher98/Extractors.kt
@@ -1,31 +1,17 @@
 package com.phisher98
 
-import android.annotation.SuppressLint
-import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.google.gson.Gson
-import com.google.gson.annotations.SerializedName
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.USER_AGENT
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.base64Encode
-import com.lagradost.cloudstream3.extractors.Rabbitstream
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
-import org.json.JSONArray
-import java.net.URL
-import java.security.MessageDigest
-import java.util.Base64
-import javax.crypto.Cipher
-import javax.crypto.spec.IvParameterSpec
-import javax.crypto.spec.SecretKeySpec
-import kotlin.collections.toByteArray
-import kotlin.emptyArray
-import kotlin.math.roundToInt
 
 class Videostr : ExtractorApi() {
     override val name = "Videostr"
@@ -45,8 +31,8 @@ class Videostr : ExtractorApi() {
             "Referer" to mainUrl,
             "User-Agent" to USER_AGENT
         )
-
         val id = url.substringAfterLast("/").substringBefore("?")
+
         val html= app.get(url, referer = url).text
         val regex = Regex("""\b[a-zA-Z0-9]{48}\b""")
         val hash = regex.find(html)?.value
@@ -55,16 +41,12 @@ class Videostr : ExtractorApi() {
 
         val json = app.get(apiUrl, headers = headers).text
         val response = Gson().fromJson(json, MediaData::class.java)
-        Log.d("Phisher",response.toJson())
-        val key = app.get("https://raw.githubusercontent.com/yogesh-hacker/MegacloudKeys/refs/heads/main/keys.json")
-            .parsedSafe<Megakey>()?.vidstr ?: return
+        val m3u8Url = response.sources.first().file
 
-        val decryptedJson = decryptOpenSSL(response.sources, key)
-        Log.d("Phisher",decryptedJson.toJson())
+        val m3u8Headers = mapOf(
+            "Referer" to "https://videostr.net/",
+        )
 
-        val m3u8Url = parseSourceJson(decryptedJson).firstOrNull()?.file ?: return
-
-        val m3u8Headers = mapOf("Referer" to mainUrl, "Origin" to mainUrl)
         generateM3u8(name, m3u8Url, mainUrl, headers = m3u8Headers).forEach(callback)
 
         response.tracks
@@ -74,61 +56,26 @@ class Videostr : ExtractorApi() {
             }
     }
 
+
     data class MediaData(
-        val sources: String,
+        val sources: List<Source>,
         val tracks: List<Track>,
         val encrypted: Boolean,
-        @SerializedName("_f") val f: String,
-        val server: Int
+        @JsonProperty("_f")
+        val f: String,
+        val server: Long,
+    )
+
+    data class Source(
+        val file: String,
+        val type: String,
     )
 
     data class Track(
         val file: String,
         val label: String,
         val kind: String,
-        @SerializedName("default") val isDefault: Boolean = false
+        val default: Boolean,
+        val s: String,
     )
-
-    data class Megakey(val mega: String, val rabbit: String,val vidstr: String)
-    data class Source2(val file: String, val type: String)
-
-    private fun parseSourceJson(json: String): List<Source2> = runCatching {
-        val jsonArray = JSONArray(json)
-        List(jsonArray.length()) {
-            val obj = jsonArray.getJSONObject(it)
-            Source2(obj.getString("file"), obj.getString("type"))
-        }
-    }.getOrElse {
-        Log.e("parseSourceJson", "Failed to parse JSON: ${it.message}")
-        emptyList()
-    }
-
-    private fun opensslKeyIv(password: ByteArray, salt: ByteArray, keyLen: Int = 32, ivLen: Int = 16): Pair<ByteArray, ByteArray> {
-        var d = ByteArray(0)
-        var d_i = ByteArray(0)
-        while (d.size < keyLen + ivLen) {
-            d_i = MessageDigest.getInstance("MD5").digest(d_i + password + salt)
-            d += d_i
-        }
-        return d.copyOfRange(0, keyLen) to d.copyOfRange(keyLen, keyLen + ivLen)
-    }
-
-    @SuppressLint("NewApi")
-    private fun decryptOpenSSL(encBase64: String, password: String): String {
-        return runCatching {
-            val data = Base64.getDecoder().decode(encBase64)
-            require(data.copyOfRange(0, 8).contentEquals("Salted__".toByteArray()))
-            val salt = data.copyOfRange(8, 16)
-            val (key, iv) = opensslKeyIv(password.toByteArray(), salt)
-
-            val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding").apply {
-                init(Cipher.DECRYPT_MODE, SecretKeySpec(key, "AES"), IvParameterSpec(iv))
-            }
-
-            String(cipher.doFinal(data.copyOfRange(16, data.size)))
-        }.getOrElse {
-            Log.e("decryptOpenSSL", "Decryption failed: ${it.message}")
-            ""
-        }
-    }
 }
--- StreamPlay/src/main/kotlin/com/Phisher98/Extractors.kt
+++ StreamPlay/src/main/kotlin/com/Phisher98/Extractors.kt
@@ -10,7 +10,6 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
-import com.google.gson.annotations.SerializedName
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.APIHolder.getCaptchaToken
 import com.lagradost.cloudstream3.SubtitleFile
@@ -2276,21 +2275,23 @@ class Videostr : ExtractorApi() {
             "Referer" to mainUrl,
             "User-Agent" to USER_AGENT
         )
-
         val id = url.substringAfterLast("/").substringBefore("?")
-        val apiUrl = "$mainUrl/embed-1/v2/e-1/getSources?id=$id"
+
+        val html= app.get(url, referer = url).text
+        val regex = Regex("""\b[a-zA-Z0-9]{48}\b""")
+        val hash = regex.find(html)?.value
+            ?: throw Exception("No 48-character token found")
+        val apiUrl = "$mainUrl/embed-1/v3/e-1/getSources?id=$id&_k=$hash"
 
         val json = app.get(apiUrl, headers = headers).text
         val response = Gson().fromJson(json, MediaData::class.java)
+        val m3u8Url = response.sources.first().file
 
-        val key = app.get("https://raw.githubusercontent.com/yogesh-hacker/MegacloudKeys/refs/heads/main/keys.json")
-            .parsedSafe<Megakey>()?.vidstr ?: return
+        val m3u8Headers = mapOf(
+            "Referer" to "https://videostr.net/",
+        )
 
-        val decryptedJson = decryptOpenSSL(response.sources, key)
-        val m3u8Url = parseSourceJson(decryptedJson).firstOrNull()?.file ?: return
-
-        val m3u8Headers = mapOf("User-Agent" to "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36")
-        generateM3u8(name, m3u8Url, "$mainUrl/", headers = m3u8Headers).forEach(callback)
+        generateM3u8(name, m3u8Url, mainUrl, headers = m3u8Headers).forEach(callback)
 
         response.tracks
             .filter { it.kind in listOf("captions", "subtitles") }
@@ -2299,63 +2300,28 @@ class Videostr : ExtractorApi() {
             }
     }
 
+
     data class MediaData(
-        val sources: String,
+        val sources: List<Source>,
         val tracks: List<Track>,
         val encrypted: Boolean,
-        @SerializedName("_f") val f: String,
-        val server: Int
+        @JsonProperty("_f")
+        val f: String,
+        val server: Long,
+    )
+
+    data class Source(
+        val file: String,
+        val type: String,
     )
 
     data class Track(
         val file: String,
         val label: String,
         val kind: String,
-        @SerializedName("default") val isDefault: Boolean = false
+        val default: Boolean,
+        val s: String,
     )
-
-    data class Megakey(val mega: String, val rabbit: String,val vidstr: String)
-    data class Source2(val file: String, val type: String)
-
-    private fun parseSourceJson(json: String): List<Source2> = runCatching {
-        val jsonArray = JSONArray(json)
-        List(jsonArray.length()) {
-            val obj = jsonArray.getJSONObject(it)
-            Source2(obj.getString("file"), obj.getString("type"))
-        }
-    }.getOrElse {
-        Log.e("parseSourceJson", "Failed to parse JSON: ${it.message}")
-        emptyList()
-    }
-
-    private fun opensslKeyIv(password: ByteArray, salt: ByteArray, keyLen: Int = 32, ivLen: Int = 16): Pair<ByteArray, ByteArray> {
-        var d = ByteArray(0)
-        var d_i = ByteArray(0)
-        while (d.size < keyLen + ivLen) {
-            d_i = MessageDigest.getInstance("MD5").digest(d_i + password + salt)
-            d += d_i
-        }
-        return d.copyOfRange(0, keyLen) to d.copyOfRange(keyLen, keyLen + ivLen)
-    }
-
-    @SuppressLint("NewApi")
-    private fun decryptOpenSSL(encBase64: String, password: String): String {
-        return runCatching {
-            val data = Base64.getDecoder().decode(encBase64)
-            require(data.copyOfRange(0, 8).contentEquals("Salted__".toByteArray()))
-            val salt = data.copyOfRange(8, 16)
-            val (key, iv) = opensslKeyIv(password.toByteArray(), salt)
-
-            val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding").apply {
-                init(Cipher.DECRYPT_MODE, SecretKeySpec(key, "AES"), IvParameterSpec(iv))
-            }
-
-            String(cipher.doFinal(data.copyOfRange(16, data.size)))
-        }.getOrElse {
-            Log.e("decryptOpenSSL", "Decryption failed: ${it.message}")
-            ""
-        }
-    }
 }
 
 
@@ -2466,12 +2432,12 @@ internal class Molop : ExtractorApi() {
             ?.data()
             ?.substringAfter("sniff(")
             ?.substringBefore(");") ?: return
-        val cleaned = sniffScript.replace(Regex("\\[.*?\\]"), "")
+        val cleaned = sniffScript.replace(Regex("\\[.*?]"), "")
         val regex = Regex("\"(.*?)\"")
         val args = regex.findAll(cleaned).map { it.groupValues[1].trim() }.toList()
         val token = args.lastOrNull().orEmpty()
         val m3u8 = "$mainUrl/m3u8/${args[1]}/${args[2]}/master.txt?s=1&cache=1&plt=$token"
-        M3u8Helper.generateM3u8(name, m3u8, mainUrl, headers = headers).forEach(callback)
+        generateM3u8(name, m3u8, mainUrl, headers = headers).forEach(callback)
     }
 }
 
@@ -2546,7 +2512,6 @@ class BuzzServer : ExtractorApi() {
             val quality = getQualityFromName(qualityText)
             val response = app.get("$url/download", referer = url, allowRedirects = false)
             val redirectUrl = response.headers["hx-redirect"] ?: ""
-            val baseUrl = getBaseUrl(url)
 
             if (redirectUrl.isNotEmpty()) {
                 callback.invoke(
