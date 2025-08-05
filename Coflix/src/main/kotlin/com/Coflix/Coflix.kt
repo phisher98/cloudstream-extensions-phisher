55c441b5716f19df70fea560a946bd3d23057d4c
Phisher98 <mandiktest2@gmail.com>
2025-08-05 21:33:45 +0530

    coflix domain change

--- Coflix/src/main/kotlin/com/Coflix/Coflix.kt
+++ Coflix/src/main/kotlin/com/Coflix/Coflix.kt
@@ -13,7 +13,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
 class Coflix : MainAPI() {
-    override var mainUrl              = "https://coflix.mov"
+    override var mainUrl              = "https://coflix.cc"
     override var name                 = "Coflix"
     override val hasMainPage          = true
     override var lang                 = "fr"
