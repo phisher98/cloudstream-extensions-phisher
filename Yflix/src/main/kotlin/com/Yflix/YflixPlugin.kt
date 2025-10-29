28be78aa300754a67c301bd56e47a36c86804202
Phisher98 <mandiktest2@gmail.com>
2025-10-29 13:44:56 +0530

    Adding YFlix

---
+++ Yflix/src/main/kotlin/com/Yflix/YflixPlugin.kt
@@ -0,0 +1,17 @@
+package com.Yflix
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class YflixPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Yflix())
+        registerExtractorAPI(MegaUp())
+        registerExtractorAPI(Fourspromax())
+        registerExtractorAPI(Rapidairmax())
+        registerExtractorAPI(rapidshare())
+    }
+}
+
