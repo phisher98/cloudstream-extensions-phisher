b4d910051820329633b28746b0262292ad2d941c
Phisher98 <mandiktest2@gmail.com>
2025-11-25 08:58:28 +0530

    Minor Fix Bollyzone #737

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,6 +77,7 @@ subprojects {
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.13")
         implementation("org.jsoup:jsoup:1.21.2")
+        implementation("androidx.annotation:annotation:1.9.1")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
         implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
