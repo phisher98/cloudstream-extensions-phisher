9c5c98da65a3fc2429180dc136dd1a4c89b660f9
Phisher98 <mandiktest2@gmail.com>
2025-12-09 14:46:27 +0530

    Minor Fix

--- TorraStream/src/main/kotlin/com/phisher98/TorraStreamAnime.kt
+++ TorraStream/src/main/kotlin/com/phisher98/TorraStreamAnime.kt
@@ -9,6 +9,7 @@ import com.lagradost.cloudstream3.HomePageList
 import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.LoadResponse
 import com.lagradost.cloudstream3.LoadResponse.Companion.addAniListId
+import com.lagradost.cloudstream3.LoadResponse.Companion.addMalId
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.MainPageRequest
 import com.lagradost.cloudstream3.Score
@@ -209,6 +210,7 @@ open class TorraStreamAnime(private val sharedPref: SharedPreferences) : MainAPI
         return if (data.format.contains("Movie",ignoreCase = true)) {
             newMovieLoadResponse(data.getTitle(), url, TvType.AnimeMovie, href) {
                 addAniListId(id.toInt())
+                addMalId(ids.idMal)
                 this.year = data.startDate.year
                 this.plot = data.description
                 this.backgroundPosterUrl = animeMetaData?.images?.firstOrNull { it.coverType == "Fanart" }?.url ?: data.bannerImage
@@ -221,6 +223,7 @@ open class TorraStreamAnime(private val sharedPref: SharedPreferences) : MainAPI
         } else {
             newAnimeLoadResponse(data.getTitle(), url, TvType.Anime) {
                 addAniListId(id.toInt())
+                addMalId(ids.idMal)
                 addEpisodes(DubStatus.Subbed, episodes)
                 this.year = data.startDate.year
                 this.plot = data.description
