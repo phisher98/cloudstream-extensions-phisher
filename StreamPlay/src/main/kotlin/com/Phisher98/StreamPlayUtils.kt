279431f71bd536d88aba30796695890a203e537c
Phisher98 <mandiktest2@gmail.com>
2025-12-15 14:09:33 +0530

    Minor Fix

--- StreamPlay/src/main/kotlin/com/Phisher98/StreamPlayUtils.kt
+++ StreamPlay/src/main/kotlin/com/Phisher98/StreamPlayUtils.kt
@@ -1315,7 +1315,8 @@ fun getAnidbEid(jsonString: String, episodeNumber: Int?): Int? {
 @RequiresApi(Build.VERSION_CODES.O)
 fun generateVrfAES(movieId: String, userId: String): String {
     // Step 1: Derive key = SHA-256("hack_" + userId)
-    val keyData = "hack_$userId".toByteArray(Charsets.UTF_8)
+    val driveKey = base64Decode("aGU1aWRnb2JJUV8=")
+    val keyData = "$driveKey$userId".toByteArray(Charsets.UTF_8)
     val keyBytes = MessageDigest.getInstance("SHA-256").digest(keyData)
     val keySpec = SecretKeySpec(keyBytes, "AES")
     val ivSpec = IvParameterSpec(ByteArray(16))
