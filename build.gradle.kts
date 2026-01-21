0a960a44afda394475d0316c4447ba6b78619fb0
Phisher98 <mandiktest2@gmail.com>
2026-01-21 18:23:20 +0530

    Minor Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.13.0")
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
