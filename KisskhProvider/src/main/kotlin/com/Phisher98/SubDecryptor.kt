98ab2e1f3b9282140e3ac1f83e87250d89813621
Phisher98 <mandiktest2@gmail.com>
2026-01-23 03:47:45 +0530

    Minor Fix

--- KisskhProvider/src/main/kotlin/com/Phisher98/SubDecryptor.kt
+++ KisskhProvider/src/main/kotlin/com/Phisher98/SubDecryptor.kt
@@ -7,15 +7,20 @@ import javax.crypto.spec.SecretKeySpec
 
 private const val KEY = "AmSmZVcH93UQUezi"
 private const val KEY2 = "8056483646328763"
+private const val KEY3 = "sWODXX04QRTkHdlZ"
+
 
 private val IV = intArrayOf(1382367819, 1465333859, 1902406224, 1164854838)
 private val IV2 = intArrayOf(909653298, 909193779, 925905208, 892483379)
+private val IV3 = intArrayOf(946894696, 1634749029, 1127508082, 1396271183)
+
 
 
 fun decrypt(encryptedB64: String): String {
     val keyIvPairs = listOf(
         Pair(KEY.toByteArray(Charsets.UTF_8), IV.toByteArray()),
-        Pair(KEY2.toByteArray(Charsets.UTF_8), IV2.toByteArray())
+        Pair(KEY2.toByteArray(Charsets.UTF_8), IV2.toByteArray()),
+        Pair(KEY3.toByteArray(Charsets.UTF_8), IV3.toByteArray())
     )
 
     val encryptedBytes = base64DecodeArray(encryptedB64) // Decode Base64 input
