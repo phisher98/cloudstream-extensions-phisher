478a6719f2d48d6569dab26b00bc0e340d6c57eb
Phisher98 <mandiktest2@gmail.com>
2026-05-17 22:35:00 +0530

    Removing AnimeKai, YFlix, MoonTV, and UltimaBeta, along with removing metadata providers from Ultima.

--- Ultima/src/main/kotlin/com/phisher98/UltimaPlugin.kt
+++ Ultima/src/main/kotlin/com/phisher98/UltimaPlugin.kt
@@ -18,11 +18,11 @@ class UltimaPlugin : Plugin() {
 
         UltimaStorageManager.currentMetaProviders.forEach { metaProvider ->
             when (metaProvider.first) {
-                "Simkl" -> if (metaProvider.second) registerMainAPI(Simkl(this))
-                "AniList" -> if (metaProvider.second) registerMainAPI(AniList(this))
-                "MyAnimeList" -> if (metaProvider.second) registerMainAPI(MyAnimeList(this))
-                "TMDB" -> if (metaProvider.second) registerMainAPI(Tmdb(this))
-                "Trakt" -> if (metaProvider.second) registerMainAPI(Trakt(this))
+                //"Simkl" -> if (metaProvider.second) registerMainAPI(Simkl(this))
+                //"AniList" -> if (metaProvider.second) registerMainAPI(AniList(this))
+                //"MyAnimeList" -> if (metaProvider.second) registerMainAPI(MyAnimeList(this))
+                //"TMDB" -> if (metaProvider.second) registerMainAPI(Tmdb(this))
+                //"Trakt" -> if (metaProvider.second) registerMainAPI(Trakt(this))
                 else -> {}
             }
         }
