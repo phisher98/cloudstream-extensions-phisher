3bf635f79059396e4fbb197256393e7946477297
Phisher98 <mandiktest2@gmail.com>
2026-06-04 20:23:32 +0530

    Fix AnimeSalt #1035

--- Animesalt/src/main/kotlin/com/phisher98/Extractor.kt
+++ Animesalt/src/main/kotlin/com/phisher98/Extractor.kt
@@ -1,24 +1,21 @@
 package com.phisher98
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.google.gson.Gson
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.extractors.Filesim
+import com.lagradost.cloudstream3.network.WebViewResolver
+import com.lagradost.cloudstream3.newSubtitleFile
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
-import com.lagradost.cloudstream3.utils.Qualities
-import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.network.WebViewResolver
-import com.lagradost.cloudstream3.newSubtitleFile
-import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.INFER_TYPE
 import com.lagradost.cloudstream3.utils.JsUnpacker
 import com.lagradost.cloudstream3.utils.M3u8Helper
 import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
+import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.getQualityFromName
-import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.newExtractorLink
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -138,9 +135,8 @@ open class MegaPlay : ExtractorApi() {
             "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0",
             "Accept" to "*/*",
             "Accept-Language" to "en-US,en;q=0.5",
-            "Accept-Encoding" to "gzip, deflate, br, zstd",
-            "Origin" to "https://rapid-cloud.co",
-            "Referer" to "https://rapid-cloud.co/",
+            "Origin" to mainUrl,
+            "Referer" to "$mainUrl/",
             "Connection" to "keep-alive",
             "Pragma" to "no-cache",
             "Cache-Control" to "no-cache"
@@ -157,24 +153,34 @@ open class MegaPlay : ExtractorApi() {
             val id = app.get(url, headers = headers).document.selectFirst("#megaplay-player")?.attr("data-id")
 
             val apiUrl = "$mainUrl/stream/getSources?id=$id&id=$id"
-            val gson = Gson()
-            val response = try {
-                val json = app.get(apiUrl, headers).text
-                gson.fromJson(json, MegaPlay::class.java)
-            } catch (_: Exception) {
-                null
-            }
 
-            val encoded = response?.sources?.file
+            val response = runCatching {
+                app.get(apiUrl, headers).parsedSafe<MegaPlayResponse>()
+            }.getOrNull()
+
+            val m3u8 = response?.sources?.file
                 ?: throw Exception("No sources found")
-            Log.d("Phisher",encoded)
-            val m3u8: String = encoded
 
-            M3u8Helper.generateM3u8(name, m3u8, mainUrl, headers = mainheaders).forEach(callback)
+            generateM3u8(
+                name,
+                m3u8,
+                mainUrl,
+                headers = mainheaders
+            ).forEach(callback)
 
             response.tracks.forEach { track ->
+                val file = track.file ?: return@forEach
+                val label = track.label ?: "Unknown"
+
                 if (track.kind == "captions" || track.kind == "subtitles") {
-                    subtitleCallback(newSubtitleFile(track.label, track.file))
+                    subtitleCallback(
+                        newSubtitleFile(label, file)
+                        {
+                            this.headers = mapOf(
+                                "Referer" to "$mainUrl/"
+                            )
+                        }
+                    )
                 }
             }
         } catch (e: Exception) {
@@ -190,8 +196,8 @@ open class MegaPlay : ExtractorApi() {
             """.trimIndent()
 
             val m3u8Resolver = WebViewResolver(
-                interceptUrl = Regex("""master\.m3u8"""),
-                additionalUrls = listOf(Regex("""master\.m3u8""")),
+                interceptUrl = Regex("""\.m3u8"""),
+                additionalUrls = listOf(Regex("""\.m3u8""")),
                 script = jsToClickPlay,
                 scriptCallback = { result -> Log.d("Megacloud", "JS Result: $result") },
                 useOkhttp = false,
@@ -224,36 +230,28 @@ open class MegaPlay : ExtractorApi() {
         }
     }
 
-    data class MegaPlay(
-        val sources: Sources,
-        val tracks: List<Track>,
-        val t: Long,
-        val intro: Intro,
-        val outro: Outro,
-        val server: Long,
+    data class MegaPlayResponse(
+        @JsonProperty("sources")
+        val sources: Sources? = null,
+        @JsonProperty("tracks")
+        val tracks: List<Track> = emptyList()
     )
 
     data class Sources(
-        val file: String,
+        @JsonProperty("file")
+        val file: String? = null
     )
 
     data class Track(
-        val file: String,
-        val label: String,
-        val kind: String,
-        val default: Boolean?,
-    )
-
-    data class Intro(
-        val start: Long,
-        val end: Long,
-    )
-
-    data class Outro(
-        val start: Long,
-        val end: Long,
+        @JsonProperty("file")
+        val file: String? = null,
+        @JsonProperty("label")
+        val label: String? = null,
+        @JsonProperty("kind")
+        val kind: String? = null
     )
 }
+
 
 open class Abyass : ExtractorApi() {
     override var name = "Abyass"
