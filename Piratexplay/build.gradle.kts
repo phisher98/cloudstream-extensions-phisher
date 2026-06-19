3cb983778e054086ea42847ec3cd5aef00350561
Phisher98 <mandiktest2@gmail.com>
2026-06-19 13:55:30 +0530

    Minor Fix

--- Piratexplay/build.gradle.kts
+++ Piratexplay/build.gradle.kts
@@ -22,7 +22,7 @@ cloudstream {
         "Cartoon"
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=piratexplay.cc&sz=%size%"
+    iconUrl = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/Icons/piratexplay.png"
 
     isCrossPlatform = true
 }
